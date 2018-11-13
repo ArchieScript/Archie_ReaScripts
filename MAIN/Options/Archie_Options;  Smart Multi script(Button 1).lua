6686a907aeb1db09743bbcfafdc0c7c1657ca26a
Archie <36672393+ArchieScript@users.noreply.github.com>
2018-11-13 21:34:12 +0800

    +!

--- MAIN/Options/Archie_Options;  Smart Multi script(Button 1).lua
+++ MAIN/Options/Archie_Options;  Smart Multi script(Button 1).lua
@@ -2,7 +2,7 @@
    * Category:    Options
    * Description: Smart Multi script(Button 1)
    * Author:      Archie
-   * Version:     1.02
+   * Version:     1.2.1
    * AboutScript: Run the script with the keyboard shortcut Ctrl + Shift + Alt + Click
    *              To display help, enter the word in the first field 'help'
    * О скрипте:   Запустите скрипт сочетанием клавиш  Ctrl + Shift + Alt + Click
