31492f14b4b2d7aad536fb5c8ebb8334c74cd371
Archie <36672393+ArchieScript@users.noreply.github.com>
2018-11-15 16:35:28 +0800

    +

---
+++ Functions/NOA_Function_lua.lua
@@ -0,0 +1,33 @@
+--[[
+   * Category:    Function
+   * Description: A_Function_lua
+   * Author:      Archie
+   * Version:     1.0
+   * About:       Functions for use with some scripts Archie
+   *              BLA BLA BLA BLA BLA BLA BLA BLA BLA BLA 
+   * О скрипте:   Функции для использования с некоторыми скриптами Archie
+   *              БЛА БЛА БЛА БЛА БЛА БЛА БЛА БЛА БЛА БЛА БЛА БЛА БЛА 
+   * Changelog:   + Test
+--======================]]
+
+
+
+    --======================================================================================
+    --///////////  FUNCTIONS  \\\\\\\\\\\\  FUNCTIONS  ////////////  FUNCTIONS  \\\\\\\\\\\\
+    --======================================================================================
+
+
+
+
+    --===================
+	local Arc_Module = {}
+    --===================
+
+
+
+--   * provides:    [nomain].
+
+
+    --===============
+    return Arc_Module
+    --===============
