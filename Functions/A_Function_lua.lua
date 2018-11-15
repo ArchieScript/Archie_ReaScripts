b1f041a680b132a21741ed295c839b70d4ab995e
Archie <36672393+ArchieScript@users.noreply.github.com>
2018-11-15 16:16:29 +0800

    +

--- Functions/A_Function_lua.lua
+++ Functions/A_Function_lua.lua
@@ -2,7 +2,7 @@
    * Category:    Functions
    * Description: A_Function_lua
    * Author:      Archie
-   * Version:     1.06
+   * Version:     1.07
    * AboutScript: Functions for use with some scripts Archie
    * О скрипте:   Функции для использования с некоторыми скриптами Archie
    * Provides:    [nomain] .
