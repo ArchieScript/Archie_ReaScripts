f48a7477158b9389579cc778931dc61d63845539
Archie <36672393+ArchieScript@users.noreply.github.com>
2018-11-15 16:46:29 +0800

    +

--- Functions/NOA_Function_lua.lua
+++ Functions/NOA_Function_lua.lua
@@ -2,10 +2,10 @@
    * Category:    Function
    * Description: NOA_Function_lua
    * Author:      Archie
-   * Version:     1.01
+   * Version:     1.02
    * About:       Functions for use with some scripts Archie \n
    *              BLA BLA BLA BLA BLA BLA BLA BLA BLA BLA 
-   * О скрипте    Функции для использования с некоторыми скриптами Archie
+   *              Функции для использования с некоторыми скриптами Archie
    *              БЛА БЛА БЛА БЛА БЛА БЛА БЛА БЛА БЛА БЛА БЛА БЛА БЛА 
    * Changelog:   + Test
 --======================]]
