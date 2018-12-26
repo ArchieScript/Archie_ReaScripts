72386fd590b00d3c817ab29d47b551dda7c2b7ca
ArchieScript <36672393+ArchieScript@users.noreply.github.com>
2018-12-27 02:25:04 +0800

    +

--- Functions/Arc_Function_lua.lua
+++ Functions/Arc_Function_lua.lua
@@ -2,7 +2,7 @@
    * Category:    Function
    * Description: Arc_Function_lua
    * Author:      Archie
-   * Version:     2.0.8
+   * Version:     2.0.9
    * AboutScript: Functions for use with some scripts Archie
    * О скрипте:   Функции для использования с некоторыми скриптами Archie
    * Provides:    [nomain].
@@ -11,8 +11,10 @@
    * Changelog:   
    *              + no_undo()
    *              + Action();
-   *              + HelpWindowWhenReRunning(BottonText,but,reset);
    *              + SetToggleButtonOnOff(numb); 0 or 1
+   *              + HelpWindowWhenReRunning(BottonText,but,reset);
+   *              + DeleteMediaItem(item);
+   *              + GetSampleNumberPosValue(take,SkipNumberOfSamplesPerChannel,FeelVolumeOfItem);
    *              + SetMediaItemLeftTrim2(position,item)
    *              + Save_Selected_Items_GuidSlot(Slot);
    *              + Restore_Selected_Items_GuidSlot(Slot,clean);--clean = true или 1 - чтобы зачистить
@@ -52,7 +54,7 @@
     ------------- http://НЕ_ЗАБУДЬ_ОБНОВИТЬ ---------------------------------------------                                --###
     -------НЕ ЗАБУДЬ ОБНОВИТЬ--------НЕ ЗАБУДЬ ОБНОВИТЬ--------НЕ ЗАБУДЬ ОБНОВИТЬ--------                                --###
     function Arc_Module.VersionArc_Function_lua(version,ScriptPath,ScriptName);                                          --###
-        local ver_fun = "2.0.8"  --<<<--НЕ ЗАБУДЬ ОБНОВИТЬ <<<                                                           --###
+        local ver_fun = "2.0.9"  --<<<--НЕ ЗАБУДЬ ОБНОВИТЬ <<<                                                           --###
         local v = ver_fun:gsub("%D", "");                                                                                --###
         if v < version:gsub("%D", "") then                                                                               --###
             reaper.ClearConsole()                                                                                        --###
@@ -78,9 +80,21 @@
 
 
 
-    --======================================================================================
-    --///////////  FUNCTIONS  \\\\\\\\\\\\  FUNCTIONS  ////////////  FUNCTIONS  \\\\\\\\\\\\
-    --======================================================================================
+
+    --=================================================================================================
+    --                                                                                               --
+    --          #########  ##     ##  ## ####      #####     ##       ##    #####    # #####    ##   --
+    --         #########  ##     ##  ########    #######    ##       ##   ##   ##   ########   ##    --
+    --        ##         ##     ##  ##     ##  ##     ##   ##           ##     ##  ##     ##  ##     --
+    --       ##         ##     ##  ##     ##  ##         #####     ##  ##     ##  ##     ##  ##      --
+    --      #########  ##     ##  ##     ##  ##          ##       ##  ##     ##  ##     ##  ##       --
+    --     #########  ##     ##  ##     ##  ##     ##   ##   ##  ##  ##     ##  ##     ##            --
+    --    ##          #######   ##     ##   #######     #####   ##   ##   ##   ##     ##  ##         --
+    --   ##           #####    ##     ##    #####       ###    ##    #####    ##     ##  ##          --
+    --                                                                                               --
+    --=================================================================================================
+
+
 
 
 
@@ -123,7 +137,6 @@
     -- УСТАНОВИТЬ ПЕРЕКЛЮЧАТЕЛЬ ВКЛ ВЫКЛ (ПОДСВЕТКА КНОПКИ)
     -- Set Toggle Button On Off
     --====End===============End===============End===============End===============End====
-
 
 
 
@@ -172,6 +185,112 @@
     -- Help Window When Re Running Script 
     --====End===============End===============End===============End===============End====
 
+
+
+
+
+    ------------ DeleteMediaItem(item); -------------------------------------------------
+    function Arc_Module.DeleteMediaItem(item);
+        if item then;
+            local tr = reaper.GetMediaItem_Track(item);
+            reaper.DeleteTrackMediaItem(tr,item);
+        end;
+    end;
+    -- УДАЛИТЬ ЭЛЕМЕНТ МУЛЬТИМЕДИА
+    -- Delete Media Item
+    --====End===============End===============End===============End===============End====
+
+
+
+
+
+    -------- GetSampleNumberPosValue(take,SkipNumberOfSamplesPerChannel,FeelVolumeOfItem); ---------
+    function Arc_Module.GetSampleNumberPosValue(take,SkipNumberOfSamplesPerChannel,FeelVolumeOfItem);
+    
+        SkipNumberOfSamplesPerChannel = math.floor(SkipNumberOfSamplesPerChannel+0.5);
+        if not tonumber(SkipNumberOfSamplesPerChannel) then SkipNumberOfSamplesPerChannel = 0 end;
+        if not take or reaper.TakeIsMIDI(take)then return false,false,false,false,false,false,false end;
+        local item = reaper.GetMediaItemTake_Item(take);
+        -----
+        -- Reset Play Rate ----------------------------------------------------------
+        local PlayRate_Reset =  reaper.GetMediaItemTakeInfo_Value(take,"D_PLAYRATE");
+        local Item_len_Reset = reaper.GetMediaItemInfo_Value(item,"D_LENGTH");
+        reaper.SetMediaItemInfo_Value(item,"D_LENGTH",Item_len_Reset * PlayRate_Reset);
+        reaper.SetMediaItemTakeInfo_Value(take,"D_PLAYRATE",1);
+        --------------------------------------------------------
+        local item_pos = reaper.GetMediaItemInfo_Value(item,"D_POSITION");
+        local item_len = reaper.GetMediaItemInfo_Value(item,"D_LENGTH");
+        local accessor = reaper.CreateTakeAudioAccessor(take);
+        local source = reaper.GetMediaItemTake_Source(take);
+        local samplerate = reaper.GetMediaSourceSampleRate(source);
+        local numchannels = reaper.GetMediaSourceNumChannels(source);
+        local item_len_idx = math.ceil(item_len);
+        -----------------------------------------
+        local CountSamples_OneChannel  = math.floor(item_len*samplerate+1);
+        local CountSamples_AllChannels = math.floor(item_len*samplerate+1)*numchannels;
+        -------------------------------------------------------------------------------
+        local NumberSamplesOneChan  = {};
+        local NumberSamplesAllChan  = {};
+        local Sample_min            = {};
+        local Sample_max            = {};
+        local TimeSample            = {};
+        ---------------------------------
+        local breakX;
+        -------------
+        for i1 = 1, item_len_idx do
+            local buffer = reaper.new_array(samplerate*numchannels); -- 1 sec
+            local Accessor_Samples = reaper.GetAudioAccessorSamples(
+                                                        accessor   , -- accessor
+                                                        samplerate , -- samplerate
+                                                        numchannels, -- numchannels
+                                                        i1-1       , -- starttime_sec
+                                                        samplerate , -- numsamplesperchannel
+                                                        buffer     ) -- reaper.array samplebuffer
+            -------------------------------------------------------------------------------------
+            local ContinueCounting = (i1-1) * samplerate; -- Продолжить Подсчет
+            -------------------------------------------------------------------
+            for i2 = 1, samplerate*numchannels,numchannels*(SkipNumberOfSamplesPerChannel+1) do;
+    
+                -- / min max sample from all channels /------------
+                local Sample_min_all_channels = 9^99;
+                local Sample_max_all_channels = 0;
+                for i3 = 1, numchannels do;
+                    local Sample = math.abs(buffer[i2+(i3-1)]);
+                    Sample_min_all_channels = math.min(Sample,Sample_min_all_channels);
+                    Sample_max_all_channels = math.max(Sample,Sample_max_all_channels);
+                end;
+                -----
+                ---/ Feel volume of item - Чувствительность к громкости элемента /---
+                if FeelVolumeOfItem == true then;
+                    Sample_min_all_channels = Sample_min_all_channels*reaper.GetMediaItemInfo_Value(item, "D_VOL");
+                    Sample_max_all_channels = Sample_max_all_channels*reaper.GetMediaItemInfo_Value(item, "D_VOL");
+                end;
+                -----
+                Sample_min[#Sample_min+1] = Sample_min_all_channels;
+                Sample_max[#Sample_max+1] = Sample_max_all_channels;
+                ----------------------------------------------------
+                NumberSamplesAllChan[#NumberSamplesAllChan+1] = (i2 + ContinueCounting);
+                NumberSamplesOneChan[#NumberSamplesOneChan+1] = math.floor(((i2 + ContinueCounting)/numchannels)+0.5);
+                ------------------------------------------------------------------------------------------------------
+                TimeSample[#TimeSample+1] = ((i2-1)/numchannels+ContinueCounting)/samplerate/PlayRate_Reset + item_pos
+                ------------------------------------------------------------------------------------------------------
+                if #TimeSample*(SkipNumberOfSamplesPerChannel+1) >= CountSamples_OneChannel then breakX = 1 break end;
+            end;
+            buffer.clear();
+            if breakX == 1 then break end;
+        end
+        reaper.DestroyAudioAccessor(accessor);
+        -----
+        -- Restore Play Rate ----------------------------------------------------
+        reaper.SetMediaItemInfo_Value(item,"D_LENGTH",item_len / PlayRate_Reset);
+        reaper.SetMediaItemTakeInfo_Value(take,"D_PLAYRATE",PlayRate_Reset);
+        --------------------------------------------------------------------
+        return CountSamples_AllChannels,CountSamples_OneChannel,NumberSamplesAllChan,
+               NumberSamplesOneChan,Sample_min,Sample_max,TimeSample;---------------
+    end;----------------------------------------------------------------------------
+    -- ПОЛУЧИТЬ У ОБРАЗЦА НОМЕР ЗНАЧЕНИЕ ПОЗИЦИЮ
+    -- Get Sample Number Position Value
+    --====End===============End===============End===============End===============End====
 
 
 
