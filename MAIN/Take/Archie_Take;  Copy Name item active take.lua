9996e376a4765437ce3266735110290697e3da80
ArchieScript <36672393+ArchieScript@users.noreply.github.com>
2019-01-17 23:00:39 +0800

    +

---
+++ MAIN/Take/Archie_Take;  Copy Name item active take.lua
@@ -0,0 +1,91 @@
+--[[
+   * Category:    Take
+   * Description: Copy Name item active take
+   * Author:      Archie
+   * Version:     1.0
+   * AboutScript: Copy Name item active take
+   * О скрипте:   Скопировать имя элемента активного тейка
+                  востановить с помощью: 
+				                        Paste Name to selected items active take
+   * GIF:         ---
+   * Website:     http://forum.cockos.com/showthread.php?t=212819
+   *              http://rmmedia.ru/threads/134701/
+   * Donation:    http://money.yandex.ru/to/410018003906628
+   * Customer:    YuriOl(RMM)
+   * Gave idea:   YuriOl(RMM)
+   * Changelog:   +  initialе / v.1.0 [17.01.2019]
+   
+   ===========================================================================================\
+   -------------SYSTEM REQUIREMENTS:-------/-------СИСТЕМНЫЕ ТРЕБОВАНИЯ:----------------------|
+   ===========================================================================================|
+   + Reaper v.5.963 -----------| http://www.reaper.fm/download.php -------|(and above |и выше)|
+   + SWS v.2.9.7 --------------| http://www.sws-extension.org/index.php --|(and above |и выше)|
+   - ReaPack v.1.2.2 ----------| http://reapack.com/repos ----------------|(and above |и выше)|
+   + Arc_Function_lua v.2.1.7 -| Repository - Archie-ReaScripts  http://clck.ru/EjERc |и выше)|
+   - reaper_js_ReaScriptAPI64 -| Repository - ReaTeam Extensions http://clck.ru/Eo5Nr |и выше)|
+                                                                 http://clck.ru/Eo5Lw |и выше)|
+   - Visual Studio С++ 2015 ---| --------- http://clck.ru/Eq5o6 ----------|(and above |и выше)|
+--===========================================================================================]]
+
+
+
+    --======================================================================================
+    --////////////// SCRIPT \\\\\\\\\\\\\\  SCRIPT  //////////////  SCRIPT  \\\\\\\\\\\\\\\\
+    --======================================================================================
+
+
+
+
+    --Mod ##########  Mod  ##########  Mod  ########  Mod  ##########  Mod  ##########  Mod  ##########  Mod  ################
+    local function GetSubdirectoriesUpToLevelFive(Path);----------------------------------------------------------------------#
+      local T,Enu,s,h = {Path},reaper.EnumerateSubdirectories,"\\",math.huge;--------------------------------------------------#
+      for i=0,h do;f1 = Enu(Path,i);if f1 then;T[#T+1]=Path..s..f1;-------------------------------------------------------------#
+        for i2=0,h do;f2=Enu(Path..s..f1,i2)if f2 then T[#T+1]=Path..s..f1..s..f2;-----------------------------------------------#
+          for i3=0,h do;f3=Enu(Path..s..f1..s..f2,i3)if f3 then T[#T+1]=Path..s..f1..s..f2..s..f3;--------------------------------#
+            for i4=0,h do;f4 = Enu(Path..s..f1..s..f2..s..f3,i4)if f4 then T[#T+1]=Path..s..f1..s..f2..s..f3..s..f4;---------------#
+              for i5=0,h do;f5=Enu(Path..s..f1..s..f2..s..f3..s..f4,i5)if f5 then T[#T+1]=Path..s..f1..s..f2..s..f3..s..f4..s..f5;--#
+              -----------------------------------------------------------------------------------------------------------------------#
+              end;if not f5 then break end;end;-- #########  ##     ##  ## ####      #####     ##       ##    #####    # #####    ## #
+            end;if not f4 then break end;end;--- #########  ##     ##  ########    #######    ##       ##   ##   ##   ########   ##  #
+          end;if not f3 then break end;end;---- ##         ##     ##  ##     ##  ##     ##   ##           ##     ##  ##     ##  ##   #
+        end;if not f2 then break end;end;----- ##         ##     ##  ##     ##  ##         #####     ##  ##     ##  ##     ##  ##    #
+      end;if not f1 then return T end;end;--- #########  ##     ##  ##     ##  ##          ##       ##  ##     ##  ##     ##  ##     #
+    end;------------------------------------ #########  ##     ##  ##     ##  ##     ##   ##   ##  ##  ##     ##  ##     ##          #
+    --------------------------------------- ##          #######   ##     ##   #######     #####   ##   ##   ##   ##     ##  ##       #
+    local function GetModule(Path,file);-- ##           #####    ##     ##    #####       ###    ##    #####    ##     ##  ##        #
+    local FolPath,mod,Way = GetSubdirectoriesUpToLevelFive(Path);--------------------------------------------------------------------#
+    FolPath[-1]=Path..'/Scripts/Archie-ReaScripts/Functions';FolPath[0]=select(2,reaper.get_action_context()):match("(.+)[\\]");-----#
+    for i=-1,#FolPath do;for i2 = 0,math.huge do; local f = reaper.EnumerateFiles(FolPath[i],i2);------------------------------------#
+    if f == file then mod=true Way=FolPath[i]break end;if not f then break end;end; if mod then return mod,Way end;end;end;----------#
+    ---------------------------------------------------------------------------------------------------------------------------------#
+    local found_mod,ScriptPath,Arc = GetModule(reaper.GetResourcePath():gsub('%\\','/'),"Arc_Function_lua.lua")---------------------#
+    if not found_mod then reaper.ClearConsole()reaper.ShowConsoleMsg('Missing file "Arc_Function_lua",\nDownload from'..-----------#
+    'repository Archie-ReaScript and put in resources of Reaper.\nОтсутствует файл "Arc_Function_lua",\nСкачайте из '..-----------#
+    'репозитория Archie-ReaScript и поместите в ресурсы Reaper') return end------------------------------------------------------#
+    package.path = package.path..";"..ScriptPath.."/?.lua"----------------------------------------------------------------------#
+    Arc = require "Arc_Function_lua"-------------------------------------------------------------------------------------------#
+    Arc.VersionArc_Function_lua("2.1.7",ScriptPath,"")------------------------------------------------------------------------#
+    --=====================================================================================================================--#
+    --####################################################################################################################### 
+
+
+
+
+    local SelItem = reaper.GetSelectedMediaItem(0,0);
+    if not SelItem then Arc.no_undo() return end;
+
+    reaper.Undo_BeginBlock();
+
+    local HasExtState = reaper.HasExtState("{section_ArcԖCopy¶NameԖTake_ѨԖX}","{key_ArcԖCopyԖNaneԖTake_ѨԖX}");
+    if HasExtState then;
+        reaper.DeleteExtState("{section_ArcԖCopy¶NameԖTake_ѨԖX}","{key_ArcԖCopyԖNaneԖTake_ѨԖX}",false);
+    end;
+
+    local ActiveTake = reaper.GetActiveTake(SelItem);
+    local _,stringNeedBig = reaper.GetSetMediaItemTakeInfo_String(ActiveTake,"P_NAME","",0);
+
+    reaper.SetExtState("{section_ArcԖCopy¶NameԖTake_ѨԖX}","{key_ArcԖCopyԖNaneԖTake_ѨԖX}",stringNeedBig,false);
+
+    reaper.Undo_EndBlock('Copy Name item active take',-1);
+    
+
