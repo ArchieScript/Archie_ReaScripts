4e20074a448ab7769c2b0e70ebf49d9371a14106
ArchieScript <ArchieScript@users.noreply.github.com>
2019-02-16 17:48:14 +0800

    +!

--- MAIN/Grid/Archie_Grid;  Free movement of item(s) when snap to grid is on(`).lua
+++ MAIN/Grid/Archie_Grid;  Free movement of item(s) when snap to grid is on(`).lua
@@ -2,7 +2,7 @@
    * Category:    Grid
    * Description: Free movement of item(s) when snap to grid is on
    * Author:      Archie
-   * Version:     1.04
+   * Version:     1.05
    * AboutScript: Free movement of item(s) when snap to grid is enabled
    *              NOTE THE SETTINGS BELOW
    * О скрипте:   Свободное перемещение элементов при включенной привязке к сетке
@@ -29,10 +29,10 @@
    + Reaper v.5.962 -----------| http://www.reaper.fm/download.php -------|(and above |и выше)|
    + SWS v.2.9.7 --------------| http://www.sws-extension.org/index.php --|(and above |и выше)|
    - ReaPack v.1.2.2 ----------| http://reapack.com/repos ----------------|(and above |и выше)|
-   - Arc_Function_lua v.1.1.6 -| Repository - Archie-ReaScripts  http://clck.ru/EjERc |и выше)|
-   + reaper_js_ReaScriptAPI64 -| Repository - ReaTeam Extensions http://clck.ru/Eo5Nr |и выше)|
+   + Arc_Function_lua v.2.2.5 -| Repository - Archie-ReaScripts  http://clck.ru/EjERc |и выше)|
+   + reaper_js_ReaScriptAPI.. -| Repository - ReaTeam Extensions http://clck.ru/Eo5Nr |и выше)|
                                                                  http://clck.ru/Eo5Lw |и выше)|
-   + Visual Studio С++ 2015 ---| --------- http://clck.ru/Eq5o6 ----------|(and above |и выше)|
+   ? Visual Studio С++ 2015 ---| --------- http://clck.ru/Eq5o6 ----------|(and above |и выше)|
    ==========================================================================================]]
 
 
@@ -108,7 +108,7 @@
     reaper.GetResourcePath();package.path=Fun.."/?.lua"..";"..scr.."/?.lua"..";"..dir.."/?.lua"..";"..package.path;Load,Arc=pcall(require,"Arc_Function_lua");
     if not Load then reaper.MB('Missing file "Arc_Function_lua",\nDownload from repository Archie-ReaScript and put in\n'..Fun..'\n\n'..'Отсутствует '..--====
     'файл "Arc_Function_lua",\nСкачайте из репозитория Archie-ReaScript и поместите в \n'..Fun,"Error.",0)return end;--=======================================
-    if not Arc.VersionArc_Function_lua("2.2.2",Fun,"")then Arc.no_undo() return end;--==================================== FUNCTION MODULE FUNCTION ==========
+    if not Arc.VersionArc_Function_lua("2.2.5",Fun,"")then Arc.no_undo() return end;--==================================== FUNCTION MODULE FUNCTION ==========
     --==================================▲=▲=▲=================================================================================================================
 
 
