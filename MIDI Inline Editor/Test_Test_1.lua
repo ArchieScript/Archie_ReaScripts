59f0e57bd56f5e97e6e0a4dbc98e82c32f363572
ArchieScript <ArchieScript@users.noreply.github.com>
2019-03-04 14:17:53 +0800

    &&&

---
+++ MIDI Inline Editor/Test_Test_1.lua
@@ -0,0 +1,46 @@
+--[[
+   * Category:    Test
+   * Description: Test
+   * Author:      Archie
+   * Version:     1.0
+   * AboutScript: Test
+   * О скрипте:   ---
+   * GIF:         ---
+   * Website:     http://forum.cockos.com/showthread.php?t=212819
+   *              http://rmmedia.ru/threads/134701/
+   * Donation:    http://money.yandex.ru/to/410018003906628
+   * Customer:    ---
+   * Gave idea:   ---
+   * Changelog:   ---
+   * Provides     
+   *              [main] . > Test_Test_1.lua
+   *              [main] . > Test_Test_2.lua
+   *              [main] . > Test_Test_3.lua
+   *              [main] . > Test_Test_4.lua
+   *              [main] . > Test_Test_5.lua
+   
+
+   --========================================================================================
+   --///// SYSTEM REQUIREMENTS: \\\\\ СИСТЕМНЫЕ ТРЕБОВАНИЯ: ///// SYSTEM REQUIREMENTS: \\\\\\
+   ----------------------------------------------------------------------------------------||
+   + Reaper v.5.967 +           --| http://www.reaper.fm/download.php                      ||
+   - SWS v.2.10.0 +             --| http://www.sws-extension.org/index.php                 ||
+   - ReaPack v.1.2.2 +          --| http://reapack.com/repos                               ||
+   - Arc_Function_lua v.2.3.0 + --| Repository - Archie-ReaScripts  http://clck.ru/EjERc   ||
+   - reaper_js_ReaScriptAPI64   --| Repository - ReaTeam Extensions http://clck.ru/Eo5Nr   ||
+                                                                    http://clck.ru/Eo5Lw   ||
+   - Visual Studio С++ 2015     --|  http://clck.ru/Eq5o6                                  ||
+   ----------------------------------------------------------------------------------------||
+   --\\\\\ СИСТЕМНЫЕ ТРЕБОВАНИЯ: ///// SYSTEM REQUIREMENTS: \\\\\ СИСТЕМНЫЕ ТРЕБОВАНИЯ: /////
+   ========================================================================================]]
+
+
+
+
+
+
+    --======================================================================================
+    --////////////// SCRIPT \\\\\\\\\\\\\\  SCRIPT  //////////////  SCRIPT  \\\\\\\\\\\\\\\\
+    --====================================================================================== 
+
+    
