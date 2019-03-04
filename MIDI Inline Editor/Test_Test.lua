412b62dcb320768ed87f8e076d2410d01c553a14
ArchieScript <ArchieScript@users.noreply.github.com>
2019-03-04 15:19:48 +0800

    &&&

--- MIDI Inline Editor/Test_Test.lua
+++ MIDI Inline Editor/Test_Test.lua
@@ -2,7 +2,7 @@
    * Category:    Test
    * Description: Test_Test
    * Author:      Archie
-   * Version:     1.14
+   * Version:     1.15
    * AboutScript: Test_Test
    * О скрипте:   ---
    * GIF:         ---
