078a7820ba3dcd3f5c31966230d8e1aac4e572de
ArchieScript <ArchieScript@users.noreply.github.com>
2019-04-02 14:09:47 +0800

    02 04 19

---
+++ MAIN/Track/Archie_Track;  Volume down on selected tracks by 1 db.lua
@@ -0,0 +1,66 @@
+--[[
+   * Category:    Track
+   * Description: Volume down on selected tracks by 1 db
+   * Author:      Archie
+   * Version:     1.0
+   * AboutScript: Volume down on selected tracks by 1 db
+   * О скрипте:   Уменьшение громкости на выбранных дорожках на 1 дБ
+   * GIF:         ---
+   * Website:     http://forum.cockos.com/showthread.php?t=212819
+   *              http://rmmedia.ru/threads/134701/
+   * DONATION:    http://money.yandex.ru/to/410018003906628
+   * Customer:    ---
+   * Gave idea:   muzicgrand[RMM]
+   * Changelog:   
+   *              +  initialе / v.1.0 [02042019]
+   
+   
+   -- Тест только на windows  /  Test only on windows.
+   --========================================================================================
+   --///// SYSTEM REQUIREMENTS: \\\\\ СИСТЕМНЫЕ ТРЕБОВАНИЯ: ///// SYSTEM REQUIREMENTS: \\\\\\
+   ----------------------------------------------------------------------------------------||
+    ˅ - (+) - обязательно для установки / (-) - не обязательно для установки               ||
+	 -------------------------------------------------------------------------------------||
+   (+) Reaper v.5.967 +           --| http://www.reaper.fm/download.php                    ||
+   (-) SWS v.2.10.0 +             --| http://www.sws-extension.org/index.php               ||
+   (-) ReaPack v.1.2.2 +          --| http://reapack.com/repos                             ||
+   (-) Arc_Function_lua v.2.3.1 + --| Repository - Archie-ReaScripts  http://clck.ru/EjERc ||
+   (-) reaper_js_ReaScriptAPI     --| Repository - ReaTeam Extensions http://clck.ru/Eo5Nr ||
+													   http://clck.ru/Eo5Lw   ||
+   (-) Visual Studio С++ 2015     --|  http://clck.ru/Eq5o6                                ||
+	 -------------------------------------------------------------------------------------||
+    ˄ - (+) - required for installation / (-) - not necessary for installation             ||
+   ----------------------------------------------------------------------------------------||
+   --\\\\\ СИСТЕМНЫЕ ТРЕБОВАНИЯ: ///// SYSTEM REQUIREMENTS: \\\\\ СИСТЕМНЫЕ ТРЕБОВАНИЯ: /////
+   ========================================================================================]]
+
+
+
+
+    --======================================================================================
+    --////////////// SCRIPT \\\\\\\\\\\\\\  SCRIPT  //////////////  SCRIPT  \\\\\\\\\\\\\\\\
+    --======================================================================================  
+    
+    
+    
+    ---------------------------------------------------------
+    local function no_undo();reaper.defer(function()end);end;
+    ---------------------------------------------------------
+    
+    
+    
+    local CountSelTrack = reaper.CountSelectedTracks(0);
+    if CountSelTrack == 0 then no_undo() return end;
+    
+    
+    reaper.Undo_BeginBlock();
+    
+    for i = 1, CountSelTrack do;
+	   local SelTrack = reaper.GetSelectedTrack(0, i-1);
+	   local vol = 20 * math.log(reaper.GetMediaTrackInfo_Value(SelTrack,"D_VOL"),10);
+	    reaper.SetMediaTrackInfo_Value(SelTrack,"D_VOL",(10^((vol-1)/20)));
+    end;
+    
+    reaper.Undo_EndBlock("Volume down on selected tracks by 1 db",-1);
+    
+    reaper.UpdateArrange();
