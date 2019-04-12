ab0b4c4f0d4e4762e2987d254f2fc906120a45de
ArchieScript <ArchieScript@users.noreply.github.com>
2019-04-13 00:14:34 +0800

    new

--- MIDI Editor/Archie_MidiEditor;  Unselect (left - right) wing events.lua
+++ MIDI Editor/Archie_MidiEditor;  Unselect (left - right) wing events.lua
@@ -2,7 +2,7 @@
    * Category:    MidiEditor
    * Description: Unselect (left-right*) wing events
    * Author:      Archie
-   * Version:     1.01
+   * Version:     1.0
    * AboutScript: Unselect (left-right*) wing events
    * О скрипте:   Отменить выбор (слева-справа*) событий миди
    * GIF:         ---
@@ -26,7 +26,7 @@
    (-) - not necessary for installation | (-) - не обязательно для установки
    -----------------------------------------------------------------------------------------
    (+) Reaper v.5.967 +           --| http://www.reaper.fm/download.php                     
-   (-) SWS v.2.10.0 +             --| http://www.sws-extension.org/index.php                
+   (+) SWS v.2.10.0 +             --| http://www.sws-extension.org/index.php                
    (-) ReaPack v.1.2.2 +          --| http://reapack.com/repos                              
    (-) Arc_Function_lua v.2.3.2 + --| Repository - Archie-ReaScripts  http://clck.ru/EjERc  
    (-) reaper_js_ReaScriptAPI     --| Repository - ReaTeam Extensions http://clck.ru/Eo5Nr or http://clck.ru/Eo5Lw    
@@ -40,7 +40,8 @@
     --////////////// SCRIPT \\\\\\\\\\\\\\  SCRIPT  //////////////  SCRIPT  \\\\\\\\\\\\\\\\
     --======================================================================================  
     
-
+    
+    
     
     ---------------------------------------------------------------
     local function no_undo();reaper.defer(function()return end)end;
@@ -71,39 +72,61 @@
     
     
     local Take = reaper.MIDIEditor_GetTake(MidiEditor);
-    local retval,count_notes,ccs,sysex = reaper.MIDI_CountEvts(Take);
-    if count_notes == 0 then no_undo() return end;
+    local count = ({reaper.MIDI_CountEvts(Take)})[2];
+    if count == 0 then no_undo() return end;
+    
+    reaper.MIDIEditor_LastFocused_OnCommand(40659,false);
+    
+    local FNG_Take = reaper.FNG_AllocMidiTake(Take);
+    local FNG_CountNotes = reaper.FNG_CountMidiNotes(FNG_Take);
+    if FNG_CountNotes == 0 then no_undo() return end;
     
     
+    local FNG_Note,Note_Sel,Note_Pos;
     if ScriptName == ScriptName_2 then;
-        for i = count_notes-1,0,-1 do;
-            _,sel_,_,_,endppq_,_,_,_ = reaper.MIDI_GetNote(Take,i);
-            if sel_ then;
-                 break;
+        for i = FNG_CountNotes-1,0,-1 do;
+            FNG_Note = reaper.FNG_GetMidiNote(FNG_Take, i);
+            
+            Note_Sel = reaper.FNG_GetMidiNoteIntProperty( FNG_Note,"SELECTED"); 
+            
+            Note_Pos = reaper.FNG_GetMidiNoteIntProperty( FNG_Note,"POSITION");
+            
+            if Note_Sel == 1 then;
+                break;
             end;
         end;
     elseif ScriptName == ScriptName_1 then;
-        for i = 1, count_notes do;
-            _,sel_,_,_startppq_,_,_,_,_ = reaper.MIDI_GetNote(Take,i-1);
-            if sel_ then;
-                 break;
+        for i = 1, FNG_CountNotes do;
+            FNG_Note = reaper.FNG_GetMidiNote(FNG_Take, i-1);
+            
+            Note_Sel = reaper.FNG_GetMidiNoteIntProperty( FNG_Note,"SELECTED"); 
+            
+            Note_Pos = reaper.FNG_GetMidiNoteIntProperty( FNG_Note,"POSITION");
+            
+            if Note_Sel == 1 then;
+                break;
             end;
         end;
     end;
     
     
-    for i = count_notes-1,0,-1 do;
-        local
-        retval,sel,muted,startppq,endppq,chan,pitch,vel = reaper.MIDI_GetNote(Take,i);
-        if sel then;
-            if ScriptName == ScriptName_2 then;
-                if endppq == endppq_ then;
-                    reaper.MIDI_SetNote(Take,i,false--[[sel]],muted,startppq,endppq,chan,pitch,vel,false);
-                end;
-            elseif ScriptName == ScriptName_1 then;    
-                if startppq == _startppq_ then;
-                    reaper.MIDI_SetNote(Take,i,false--[[sel]],muted,startppq,endppq,chan,pitch,vel,false);
-                end;
-            end; 
+    if Note_Sel == 1 then;
+        reaper.Undo_BeginBlock()
+        
+        for i = FNG_CountNotes-1,0,-1 do;
+        
+            local FNG_Note2 = reaper.FNG_GetMidiNote(FNG_Take, i);
+            local Note_Sel2 = reaper.FNG_GetMidiNoteIntProperty(FNG_Note2,"SELECTED");  
+            local Note_Pos2 = reaper.FNG_GetMidiNoteIntProperty(FNG_Note2,"POSITION");
+            if Note_Pos == Note_Pos2 then;
+                
+                if Note_Sel2 == 1 then;
+                    reaper.FNG_SetMidiNoteIntProperty(FNG_Note2,"SELECTED",0);
+                end;  
+            end;
         end;
+        
+        reaper.Undo_EndBlock(ScriptName:gsub("Archie_MidiEditor;  ",""),-1)
     end;
+    
+    reaper.FNG_FreeMidiTake(FNG_Take);
