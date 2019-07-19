86fb4baff2f7e945d00fcdaa81bb91bbe2c6bd7f
ArchieScript <ArchieScript@users.noreply.github.com>
2019-07-19 20:31:38 +0800

    +

---
+++ MAIN/Item/Archie_Item;  Switch item source file to previous in folder.lua
@@ -0,0 +1,249 @@
+--[[
+   * Category:    Item
+   * Description: Switch item source file to previous in folder
+   * Author:      Archie
+   * Version:     1.0
+   * AboutScript: Switch item source file to previous in folder
+   * О скрипте:   Переключить исходный файл элемента на предыдущий в папке
+   *              НЕ СПОТЫКАЕТСЯ(ЛОМАЕТСЯ) НА МИДИ ФАЙЛАХ КАК В SWS
+   * GIF:         ---
+   * Website:     http://forum.cockos.com/showthread.php?t=212819
+   *              http://rmmedia.ru/threads/134701/
+   * Donation:    http://money.yandex.ru/to/410018003906628
+   * Customer:    ---(---)
+   * Gave idea:   ---(---)
+   * Provides:    
+   *              [main] . > Archie_Item;  Switch item source file to previous in folder.lua
+   *              [main] . > Archie_Item;  Switch item source file to previous in folder(only Audio).lua
+   *              [main] . > Archie_Item;  Switch item source file to previous in folder(only Video).lua
+   *              [main] . > Archie_Item;  Switch item source file to previous in folder restore original size.lua
+   *              [main] . > Archie_Item;  Switch item source file to previous in folder restore original size(only Audio).lua
+   *              [main] . > Archie_Item;  Switch item source file to previous in folder restore original size(only Video).lua
+   * Changelog:   
+   *              v.1.0 [19.07.19] 
+   *                  initial
+   
+
+    -- Тест только на windows  /  Test only on windows.
+    --=======================================================================================
+       SYSTEM REQUIREMENTS:           |  СИСТЕМНЫЕ ТРЕБОВАНИЯ:
+    (+) - required for installation      | (+) - обязательно для установки
+    (-) - not necessary for installation | (-) - не обязательно для установки
+    -----------------------------------------------------------------------------------------
+    (+) Reaper v.5.975 +            --| http://www.reaper.fm/download.php
+    (+) SWS v.2.10.0 +              --| http://www.sws-extension.org/index.php
+    (-) ReaPack v.1.2.2 +           --| http://reapack.com/repos
+    (+) Arc_Function_lua v.2.4.7 +  --| Repository - Archie-ReaScripts  http://clck.ru/EjERc  
+    (-) reaper_js_ReaScriptAPI64    --| Repository - ReaTeam Extensions http://clck.ru/Eo5Nr or http://clck.ru/Eo5Lw    
+    (-) Visual Studio С++ 2015      --|  http://clck.ru/Eq5o6
+    =======================================================================================]] 
+
+
+
+    --======================================================================================
+    --////////////  НАСТРОЙКИ  \\\\\\\\\\\\  SETTINGS  ////////////  НАСТРОЙКИ  \\\\\\\\\\\\
+    --======================================================================================
+
+
+
+    local reset_play_rate = 0
+                       -- = 0 | СБРОСИТЬ PLAY RATE 
+                       -- = 1 | ОСТАВИТЬ ИСХОДНЫМ PLAY RATE
+                       ------------------------------------
+                       -- = 0 | RESET PLAY RATE 
+                       -- = 1 | WRITE THE SOURCE PLAY RATE
+                       ------------------------------------
+
+
+    local Start_in_Sourse = 0
+                       -- = 0 | СБРОСИТЬ START IN SOURSE
+                       -- = 1 | ОСТАВИТЬ ИСХОДНЫМ START IN SOURSE
+                       ------------------------------------------
+                       -- = 0 | RESET START IN SOURCE
+                       -- = 1 | WRITE THE SOURCE START IN SOURCE
+                       -----------------------------------------
+
+
+
+    --======================================================================================
+    --////////////// SCRIPT \\\\\\\\\\\\\\  SCRIPT  //////////////  SCRIPT  \\\\\\\\\\\\\\\\
+    --======================================================================================
+
+
+
+
+    --============================ FUNCTION MODULE FUNCTION ================================ FUNCTION MODULE FUNCTION ========================================
+    local Fun,scr,dir,MB,Arc,Load = reaper.GetResourcePath()..'/Scripts/Archie-ReaScripts/Functions',select(2,reaper.get_action_context()):match("(.+)[\\/]"),
+    reaper.GetResourcePath();package.path=Fun.."/?.lua"..";"..scr.."/?.lua"..";"..dir.."/?.lua"..";"..package.path;Load,Arc=pcall(require,"Arc_Function_lua");
+    if not Load then reaper.MB('Missing file "Arc_Function_lua",\nDownload from repository Archie-ReaScript and put in\n'..Fun..'\n\n'..'Отсутствует '..--====
+    'файл "Arc_Function_lua",\nСкачайте из репозитория Archie-ReaScript и поместите в \n'..Fun,"Error.",0)return end;--=======================================
+    if not Arc.VersionArc_Function_lua("2.4.7",Fun,"")then Arc.no_undo() return end;--==================================== FUNCTION MODULE FUNCTION ==========
+    --==================================▲=▲=▲=================================================================================================================
+    
+    
+    
+    
+    ---------------
+    local
+    ScrNameT = {
+                "Archie_Item;  Switch item source file to previous in folder.lua",
+                "Archie_Item;  Switch item source file to previous in folder(only Audio).lua",
+                "Archie_Item;  Switch item source file to previous in folder(only Video).lua",
+                "Archie_Item;  Switch item source file to previous in folder restore original size.lua",
+                "Archie_Item;  Switch item source file to previous in folder restore original size(only Audio).lua",
+                "Archie_Item;  Switch item source file to previous in folder restore original size(only Video).lua"
+               };
+    local ScrName = ({reaper.get_action_context()})[2]:match(".+[/\\](.+)");
+    local ScrNameUndo = ScrName:match("Archie.-%s%s*(.+)%.lua$");
+    
+    local AUDIO,AUDIO_MP3,ORIGINAL_SIZE;
+    
+    if ScrName == ScrNameT[1] then;
+        ----
+        AUDIO, AUDIO_MP3  = "WAVE", "MP3";
+        VIDEO  = "VIDEO";
+        ----
+    elseif ScrName == ScrNameT[2] then;
+        ----
+        AUDIO, AUDIO_MP3  = "WAVE", "MP3";
+        ----
+    elseif ScrName == ScrNameT[3] then;
+        ----
+        VIDEO  = "VIDEO";
+        ----
+    elseif ScrName == ScrNameT[4] then;
+        ----
+        AUDIO, AUDIO_MP3  = "WAVE", "MP3";
+        VIDEO  = "VIDEO";
+        ORIGINAL_SIZE = true;
+        ---- 
+    elseif ScrName == ScrNameT[5] then;
+        ----
+        AUDIO, AUDIO_MP3  = "WAVE", "MP3";
+        ORIGINAL_SIZE = true;
+        ---- 
+    elseif ScrName == ScrNameT[6] then;
+        ----
+        VIDEO  = "VIDEO";
+        ORIGINAL_SIZE = true;
+        ---- 
+    else;
+        reaper.MB("Rus:\nНеверное имя скрипта!\nИмя скрипта должно быть одно из следующих в зависимости от задачи:\n\n"..
+                  "Eng:\nInvalid script name!\nThe script name must be one of the following depending on the task:\n\n\n\n"..
+                  table.concat(ScrNameT,"\n\n")
+                  ,"Error",0);
+        Arc.no_undo() return;
+    end;
+    ---------------
+    
+    
+    
+    ----------------------------------------------
+    
+    local Count_sel_item = reaper.CountSelectedMediaItems(0);
+    if Count_sel_item == 0 then Arc.no_undo() return end;
+    
+    
+    local Countfile,Activ,Undo,Stop;
+
+    for i2 = 1, Count_sel_item do;
+
+        local item = reaper.GetSelectedMediaItem(0,i2-1);
+        local take = reaper.GetActiveTake(item);
+        local Midi_take = reaper.TakeIsMIDI(take);
+        if not Midi_take then;
+            local Path,Name = Arc.GetPathAndNameSourceMediaFile_Take(take);
+            if Path and Name then;
+                local PCM_Source = reaper.PCM_Source_CreateFromFile(Path.."/"..Name);
+                local WavMidVideo = reaper.GetMediaSourceType(PCM_Source,"");
+                if Arc.If_Equals_Or(WavMidVideo, AUDIO, AUDIO_MP3, VIDEO)then;----
+
+                    ---- Number of Files in directory ---------------
+                    for i = 1, math.huge do;
+                        local file = reaper.EnumerateFiles(Path,i-1);
+                        if file then Countfile = i else break end;
+                    end;
+                    if not Countfile then Countfile = 0 end;
+                    ----------------------------------------
+
+                    if Countfile > 0 then;
+                        ---- Replace source file --------
+                        local i = Countfile;
+                        repeat;
+
+                            local File = reaper.EnumerateFiles(Path,i-1);
+
+                            if Activ == "Active" then;
+                                if not File then File = reaper.EnumerateFiles(Path,Countfile-1);
+                                    if not Stop then i = Countfile; end;
+                                end;
+                                if File then;
+                                    Name = File;
+                                end;
+                                local ExpansionFile = string.match(Name:reverse(),".-%.");
+                                if ExpansionFile then;
+                                    local PCM_Source = reaper.PCM_Source_CreateFromFile(Path.."/"..Name);
+                                    local WavMidVideo = reaper.GetMediaSourceType(PCM_Source,"");
+                                    if Arc.If_Equals_Or(WavMidVideo, AUDIO, AUDIO_MP3, VIDEO)then;----
+                                        
+                                        local Channels = reaper.GetMediaSourceNumChannels(PCM_Source);
+                                        if Channels > 0 then;
+                                            reaper.BR_SetTakeSourceFromFile(take, Path.."/"..Name ,true);
+                                            reaper.GetSetMediaItemTakeInfo_String(take,"P_NAME",Name,1);
+                                            ----
+                                            if ORIGINAL_SIZE then;
+                                                local retval, lengthIsQN = reaper.GetMediaSourceLength(PCM_Source);
+                                                reaper.SetMediaItemLength(item,retval,true);
+                                            end;
+                                            ----
+                                             if not Undo then reaper.Undo_BeginBlock();Undo = "Active";end;
+                                            ----
+                                            break;
+                                        end;
+                                    end;
+                                end;
+                            end;
+
+                            if File == Name then;
+                                Activ = "Active";
+                            end;
+
+                            if not Stop then;
+                                if i < 0 and not Activ then Activ = "Active"; i = Countfile+1; Stop = "Active" end;
+                            elseif i < 0 and Stop then;
+                                Activ = nil;
+                            end;
+
+                        i = i - 1;
+                        until i < (-5);
+                        reaper.ClearPeakCache();
+                        ------------------------
+                    end;
+                    -------------       
+                    if Undo == "Active" then;
+                        ---- Start_in_Sourse --------
+                        if Start_in_Sourse == 0 then;
+                            reaper.SetMediaItemTakeInfo_Value(take,"D_STARTOFFS",0);
+                        end;
+                        ---- reset_play_rate --------
+                        if reset_play_rate == 0 then;
+                            reaper.SetMediaItemTakeInfo_Value(take,'D_PLAYRATE',1);
+                        end;
+                    end;
+                    --------
+                end;
+            end; 
+        end; 
+        Activ = nil; 
+        Stop = nil;
+        Name = nil;
+    end;
+           
+    if Undo == "Active" then;
+        reaper.Undo_EndBlock(ScrNameUndo,-1);
+    else;
+        Arc.no_undo();
+    end;
+    ----
+     Arc.Action(40047);--Build missing peaks
+    reaper.UpdateArrange();
