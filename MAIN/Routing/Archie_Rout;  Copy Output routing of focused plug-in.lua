20b1a7a58664006ba34f3b7a664af6fb677bcfe2
ArchieScript <ArchieScript@users.noreply.github.com>
2019-10-31 20:05:27 +0800

    +

---
+++ MAIN/Routing/Archie_Rout;  Copy Output routing of focused plug-in.lua
@@ -0,0 +1,64 @@
+--[[
+   * Тест только на windows  /  Test only on windows.
+   * Отчет об ошибке: Если обнаружите какие либо ошибки, то сообщите по одной из указанных ссылок ниже (*Website)
+   * Bug Reports: If you find any errors, please report one of the links below (*Website)
+   *
+   * Category:    Routing
+   * Description: Copy Output routing of focused plug-in
+   * Author:      Archie
+   * Version:     1.0
+   * Описание:    Копировать маршрут вывода сфокусированного плагина
+   * Website:     http://forum.cockos.com/showthread.php?t=212819
+   *              http://rmmedia.ru/threads/134701/
+   * DONATION:    http://money.yandex.ru/to/410018003906628
+   * Customer:    Дима Горелик(Rmm)
+   * Gave idea:   Дима Горелик(Rmm)
+   * Changelog:   v.1.0 [31.10.19]
+   *                  + initialе
+--]]
+    
+    
+    --======================================================================================
+    --////////////// SCRIPT \\\\\\\\\\\\\\  SCRIPT  //////////////  SCRIPT  \\\\\\\\\\\\\\\\
+    --======================================================================================
+    
+    
+    
+    local function CopyOutput_FocusedFX_Pin(NameScript,persist);
+    
+	   local retval,tracknumber,itemnumber,fxnumber = reaper.GetFocusedFX();
+	   local track = reaper.GetTrack(0,tracknumber-1);
+	   if not track then return -1 end;
+	   
+	   local t = {};
+	   if retval > 0 then;
+		  reaper.DeleteExtState(NameScript,"CopyOutputPlugin",persist);
+		  if itemnumber < 0 then;
+			 ----
+			 local _,_,outputPins = reaper.TrackFX_GetIOSize(track,fxnumber);
+			 for i = 1,outputPins do;
+				local retval, high32 = reaper.TrackFX_GetPinMappings(track,fxnumber,1,i-1);
+				t[#t+1]= "{".. i-1 .. "&" .. retval.."}";
+			 end;
+		  else;
+			 local take_numb = fxnumber >> 16;
+			 local fx_number = fxnumber & 65535;
+			 local Item = reaper.GetTrackMediaItem(track,itemnumber);
+			 local Take = reaper.GetTake(Item,take_numb);
+			 local _,_,outputPins = reaper.TakeFX_GetIOSize(Take,fx_number);
+			 
+			 for i = 1,outputPins do;
+				local retval, high32 = reaper.TakeFX_GetPinMappings(Take,fx_number,1,i-1);
+				t[#t+1]= "{".. i-1 .. "&" .. retval.."}";
+			 end; 
+		  end;
+		  t = table.concat(t);
+		  reaper.SetExtState(NameScript,"CopyOutputPlugin",t,persist);--NameScript
+	   else;
+		  return -1;
+	   end;
+    end;
+    
+    
+    CopyOutput_FocusedFX_Pin("Archie_Rout;CopyOutputRoutingOfFocusedPlugIn.lua",false);
+    reaper.defer(function()end);
---
+++ MAIN/Routing/Archie_Rout;  Paste IO routing in focused plug-in.lua
@@ -0,0 +1,115 @@
+--[[
+   * Тест только на windows  /  Test only on windows.
+   * Отчет об ошибке: Если обнаружите какие либо ошибки, то сообщите по одной из указанных ссылок ниже (*Website)
+   * Bug Reports: If you find any errors, please report one of the links below (*Website)
+   *
+   * Category:    Routing
+   * Description: Paste IO routing in focused plug-in
+   * Author:      Archie
+   * Version:     1.0
+   * Описание:    Вставить маршрутизацию ввода-вывода в сфокусированный плагин
+   * Website:     http://forum.cockos.com/showthread.php?t=212819
+   *              http://rmmedia.ru/threads/134701/
+   * DONATION:    http://money.yandex.ru/to/410018003906628
+   * Customer:    Дима Горелик(Rmm)
+   * Gave idea:   Дима Горелик(Rmm)
+   * Changelog:   v.1.0 [31.10.19]
+   *                  + initialе
+--]]
+    
+    
+    --======================================================================================
+    --////////////// SCRIPT \\\\\\\\\\\\\\  SCRIPT  //////////////  SCRIPT  \\\\\\\\\\\\\\\\
+    --======================================================================================
+    
+    
+    local function PasteInput_FocusedFX_Pin(NameScript,clean,persist);
+        local retval,tracknumber,itemnumber,fxnumber = reaper.GetFocusedFX();
+        local track = reaper.GetTrack(0,tracknumber-1);
+        if not track then return -1 end;
+        
+        if retval > 0 then;
+        
+            if itemnumber < 0 then;
+                local t = reaper.GetExtState(NameScript,"CopyInputPlugin");
+                if t ~= "" then;
+                    for var in t:gmatch("{.-}") do;
+                        local InPlug,Channel_beat = var:match("{(.-)&(.-)}");
+                        reaper.TrackFX_SetPinMappings(track,fxnumber,0,InPlug,Channel_beat,0);
+                    end;
+                    if clean == true then;
+                        reaper.DeleteExtState(NameScript,"CopyInputPlugin",persist);
+                    end;
+                end;
+            else;
+            ----
+                local take_numb = fxnumber >> 16;
+                local fx_number = fxnumber & 65535;
+                local Item = reaper.GetTrackMediaItem(track,itemnumber);
+                local Take = reaper.GetTake(Item,take_numb);
+                
+                local t = reaper.GetExtState(NameScript,"CopyInputPlugin");
+                if t ~= "" then;
+                    for var in t:gmatch("{.-}") do;
+                        local InPlug,Channel_beat = var:match("{(.-)&(.-)}");
+                        reaper.TakeFX_SetPinMappings(Take,fx_number,0,InPlug,Channel_beat,0);
+                    end;
+                    if clean == true then;
+                        reaper.DeleteExtState(NameScript,"CopyInputPlugin",persist);
+                    end;
+                end;
+                ----  
+            end;
+        else;
+            return -1;
+        end;
+    end;
+    
+    
+    
+    local function PasteOutput_FocusedFX_Pin(NameScript,clean,persist);
+        local retval,tracknumber,itemnumber,fxnumber = reaper.GetFocusedFX();
+        local track = reaper.GetTrack(0,tracknumber-1);
+        if not track then return -1 end;
+        
+        if retval > 0 then;
+      
+            if itemnumber < 0 then;
+                local t = reaper.GetExtState(NameScript,"CopyOutputPlugin");
+                if t ~= "" then;
+                    for var in t:gmatch("{.-}") do;
+                        local OutPlug,Channel_beat = var:match("{(.-)&(.-)}");
+                        reaper.TrackFX_SetPinMappings(track,fxnumber,1,OutPlug,Channel_beat,0);
+                    end;
+                    if clean == true then;
+                        reaper.DeleteExtState(NameScript,"CopyOutputPlugin",persist);
+                    end;
+                end;
+            else;
+            ----
+                local take_numb = fxnumber >> 16;
+                local fx_number = fxnumber & 65535;
+                local Item = reaper.GetTrackMediaItem(track,itemnumber);
+                local Take = reaper.GetTake(Item,take_numb);
+                
+                local t = reaper.GetExtState(NameScript,"CopyOutputPlugin");
+                if t ~= "" then;
+                    for var in t:gmatch("{.-}") do;
+                        local OutPlug,Channel_beat = var:match("{(.-)&(.-)}");
+                        reaper.TakeFX_SetPinMappings(Take,fx_number,1,OutPlug,Channel_beat,0);
+                    end;
+                    if clean == true then;
+                        reaper.DeleteExtState(NameScript,"CopyOutputPlugin",persist);
+                    end;
+                end;
+                ----  
+            end;
+        else;
+            return -1;
+        end;
+    end;
+    
+    
+    PasteInput_FocusedFX_Pin ("CopyIORoutingOfFocusedPlugIn",false,false);
+    PasteOutput_FocusedFX_Pin("CopyIORoutingOfFocusedPlugIn",false,false);
+    reaper.defer(function()end);
