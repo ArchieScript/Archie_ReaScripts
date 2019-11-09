3592c2c735cc4e5aad0ee0d3af06806c33c25bf8
ArchieScript <ArchieScript@users.noreply.github.com>
2019-11-09 14:39:25 +0800

    +

--- MAIN/Item/Archie_Item;  Stutter 1.3 items.lua
+++ MAIN/Item/Archie_Item;  Stutter 1.3 items.lua
@@ -6,16 +6,18 @@
    * Category:    Item
    * Description: Stutter 1.3 items
    * Author:      Archie
-   * Version:     1.01
+   * Version:     1.02
    * Website:     http://forum.cockos.com/showthread.php?t=212819
    *              http://rmmedia.ru/threads/134701/
    * Donation:    http://money.yandex.ru/to/410018003906628
    * Customer:    borisuperful(Rmm)
    * Gave idea:   borisuperful(Rmm)
    * Changelog:   
-   *              v.1.01 [02.10.19]
-   *                  + copying automation
+   *              v.1.02 [09.11.19]
+   *                  +  Fixed bug cleared check box (Move edit cursor when pasting/inserting media)
    
+   *              v.1.01 [02.10.19]
+   *                  + copying automation 
    *              v.1.0 [16.08.19]
    *                  + initialе
 --]]
@@ -59,6 +61,12 @@
         end;
     end;
     --clo1 = os.clock()
+    
+    local itClkMvCr = reaper.SNM_GetIntConfigVar("itemclickmovecurs",0);
+    if itClkMvCr&8 ~= 0 then;
+        reaper.SNM_SetIntConfigVar("itemclickmovecurs",itClkMvCr&~(itClkMvCr&8));
+    end;
+    
     reaper.Undo_BeginBlock();
     reaper.PreventUIRefresh(1);
     ---------------------------------------------
@@ -72,119 +80,127 @@
             table.insert(SelItemT,SelItem);
         end;
     end;
-    if #SelItemT == 0 then no_undo() return end;
+    if #SelItemT == 0 then goto END end;
+    do;
     ----
-    reaper.SelectAllMediaItems(0,0);
-    -----
-    local SelTrackT_X = {};
-    for i = 1,reaper.CountSelectedTracks(0) do;
-        table.insert(SelTrackT_X,reaper.GetSelectedTrack(0,i-1));
-    end;
-    ----
-    local EditCursor_X = reaper.GetCursorPosition();
-    local start_time_X, end_time_X = reaper.GetSet_ArrangeView2(0,0,0,0);
-    local unSelTr;
-    ----
-    if moveEnv == 0 then;
-        ---------------------------------------------
-        ::RESTART::
-        local len2;
-        for i = #SelItemT,1,-1 do;
-            local len = reaper.GetMediaItemInfo_Value(SelItemT[i],"D_LENGTH");
-            if not len2 or len == len2 then;
+        reaper.SelectAllMediaItems(0,0);
+        -----
+        local SelTrackT_X = {};
+        for i = 1,reaper.CountSelectedTracks(0) do;
+            table.insert(SelTrackT_X,reaper.GetSelectedTrack(0,i-1));
+        end;
+        ----
+        local EditCursor_X = reaper.GetCursorPosition();
+        local start_time_X, end_time_X = reaper.GetSet_ArrangeView2(0,0,0,0);
+        local unSelTr;
+        ----
+        if moveEnv == 0 then;
+            ---------------------------------------------
+            ::RESTART::
+            local len2;
+            for i = #SelItemT,1,-1 do;
+                local len = reaper.GetMediaItemInfo_Value(SelItemT[i],"D_LENGTH");
+                if not len2 or len == len2 then;
+                    local trackIt = reaper.GetMediaItem_Track(SelItemT[i]);
+                    if not len2 then; reaper.SetOnlyTrackSelected(trackIt)end;
+                    local Sel = reaper.GetMediaTrackInfo_Value(trackIt,"I_SELECTED");
+                    if Sel == 0 then;
+                        reaper.SetMediaTrackInfo_Value(trackIt,"I_SELECTED",1);
+                    end;
+                    reaper.SetMediaItemInfo_Value(SelItemT[i],"B_UISEL",1);
+                    reaper.SetMediaItemInfo_Value(SelItemT[i],"D_LENGTH",len/3);
+                    len2 = len;
+                    table.remove(SelItemT,i);
+                end;
+            end;
+            ----
+            local SecEndItem;
+            local firstEndIt = math.huge;
+            for i = 1,reaper.CountSelectedTracks(0) do;
+                local track = reaper.GetSelectedTrack(0,i-1);
+                for ii = 1,reaper.CountTrackMediaItems(track) do;
+                    local item = reaper.GetTrackMediaItem(track,ii-1);
+                    if reaper.GetMediaItemInfo_Value(item,"B_UISEL")==1 then;
+                        local pos = reaper.GetMediaItemInfo_Value(item,"D_POSITION");
+                        local len = reaper.GetMediaItemInfo_Value(item,"D_LENGTH");
+                        if pos+len < firstEndIt then firstEndIt = pos+len SecEndItem = pos+len+len end; 
+                    end
+                end;
+            end;
+            ----
+            reaper.SetEditCurPos(firstEndIt,false,false);
+            reaper.Main_OnCommand(40698,0)--C
+            reaper.Main_OnCommand(40058,0)--P
+            for i = 1, reaper.CountSelectedMediaItems(0) do;
+                table.insert(SelItemT_X,reaper.GetSelectedMediaItem(0,i-1));
+            end;
+            reaper.SetEditCurPos(SecEndItem,false,false);
+            reaper.Main_OnCommand(40058,0)--P
+            for i = 1, reaper.CountSelectedMediaItems(0) do;
+                table.insert(SelItemT_X,reaper.GetSelectedMediaItem(0,i-1));
+            end;
+            reaper.SelectAllMediaItems(0,0);
+            if #SelItemT > 0 then len2 = nil goto RESTART end;--/\
+            ---------------------------------------------
+        else;
+            ---------------------------------------------
+            for i = 1, #SelItemT do;
+                ---- 
                 local trackIt = reaper.GetMediaItem_Track(SelItemT[i]);
-                if not len2 then; reaper.SetOnlyTrackSelected(trackIt)end;
+                if not unSelTr then;
+                    reaper.SetOnlyTrackSelected(trackIt);unSelTr = true;
+                end;
+                ----
                 local Sel = reaper.GetMediaTrackInfo_Value(trackIt,"I_SELECTED");
                 if Sel == 0 then;
-                    reaper.SetMediaTrackInfo_Value(trackIt,"I_SELECTED",1);
+                    reaper.SetOnlyTrackSelected(trackIt);
                 end;
-                reaper.SetMediaItemInfo_Value(SelItemT[i],"B_UISEL",1);
+                ----
+                local pos = reaper.GetMediaItemInfo_Value(SelItemT[i],"D_POSITION");
+                local len = reaper.GetMediaItemInfo_Value(SelItemT[i],"D_LENGTH");
                 reaper.SetMediaItemInfo_Value(SelItemT[i],"D_LENGTH",len/3);
-                len2 = len;
-                table.remove(SelItemT,i);
+                reaper.SetMediaItemInfo_Value(SelItemT[i],"B_UISEL",1);
+                reaper.SetEditCurPos(pos+len/3,false,false);
+                reaper.Main_OnCommand(40698,0);--C
+                reaper.Main_OnCommand(40058,0);--P
+                table.insert(SelItemT_X,reaper.GetSelectedMediaItem(0,0));
+                reaper.Main_OnCommand(40058,0);--P
+                table.insert(SelItemT_X,reaper.GetSelectedMediaItem(0,0));
+                reaper.SelectAllMediaItems(0,0);
             end;
+            ---------------------------------------------
         end;
         ----
-        local SecEndItem;
-        local firstEndIt = math.huge;
-        for i = 1,reaper.CountSelectedTracks(0) do;
-            local track = reaper.GetSelectedTrack(0,i-1);
-            for ii = 1,reaper.CountTrackMediaItems(track) do;
-                local item = reaper.GetTrackMediaItem(track,ii-1);
-                if reaper.GetMediaItemInfo_Value(item,"B_UISEL")==1 then;
-                    local pos = reaper.GetMediaItemInfo_Value(item,"D_POSITION");
-                    local len = reaper.GetMediaItemInfo_Value(item,"D_LENGTH");
-                    if pos+len < firstEndIt then firstEndIt = pos+len SecEndItem = pos+len+len end; 
-                end
-            end;
+        reaper.SetEditCurPos(EditCursor_X,false,false);
+        reaper.GetSet_ArrangeView2(0,1,0,0,start_time_X,end_time_X);
+        if moveEnv_X then reaper.Main_OnCommand(40070,0)end;
+        
+        for i = 1, #SelItemT_X do;
+            reaper.SetMediaItemInfo_Value(SelItemT_X[i],"B_UISEL",1);
         end;
-        ----
-        reaper.SetEditCurPos(firstEndIt,false,false);
-        reaper.Main_OnCommand(40698,0)--C
-        reaper.Main_OnCommand(40058,0)--P
-        for i = 1, reaper.CountSelectedMediaItems(0) do;
-            table.insert(SelItemT_X,reaper.GetSelectedMediaItem(0,i-1));
+        
+        if #SelTrackT_X > 0 then;
+            reaper.SetOnlyTrackSelected(SelTrackT_X[1]);
+        else;
+            reaper.SetOnlyTrackSelected(reaper.GetTrack(0,0));
+            reaper.SetMediaTrackInfo_Value(reaper.GetTrack(0,0),"I_SELECTED",0);
         end;
-        reaper.SetEditCurPos(SecEndItem,false,false);
-        reaper.Main_OnCommand(40058,0)--P
-        for i = 1, reaper.CountSelectedMediaItems(0) do;
-            table.insert(SelItemT_X,reaper.GetSelectedMediaItem(0,i-1));
-        end;
-        reaper.SelectAllMediaItems(0,0);
-        if #SelItemT > 0 then len2 = nil goto RESTART end;--/\
-        ---------------------------------------------
-    else;
-        ---------------------------------------------
-        for i = 1, #SelItemT do;
-            ---- 
-            local trackIt = reaper.GetMediaItem_Track(SelItemT[i]);
-            if not unSelTr then;
-                reaper.SetOnlyTrackSelected(trackIt);unSelTr = true;
-            end;
-            ----
-            local Sel = reaper.GetMediaTrackInfo_Value(trackIt,"I_SELECTED");
+        
+        for i = 1, #SelTrackT_X do;
+            local Sel = reaper.GetMediaTrackInfo_Value(SelTrackT_X[i],"I_SELECTED");
             if Sel == 0 then;
-                reaper.SetOnlyTrackSelected(trackIt);
+                reaper.SetMediaTrackInfo_Value(SelTrackT_X[i],"I_SELECTED",1);
             end;
-            ----
-            local pos = reaper.GetMediaItemInfo_Value(SelItemT[i],"D_POSITION");
-            local len = reaper.GetMediaItemInfo_Value(SelItemT[i],"D_LENGTH");
-            reaper.SetMediaItemInfo_Value(SelItemT[i],"D_LENGTH",len/3);
-            reaper.SetMediaItemInfo_Value(SelItemT[i],"B_UISEL",1);
-            reaper.SetEditCurPos(pos+len/3,false,false);
-            reaper.Main_OnCommand(40698,0);--C
-            reaper.Main_OnCommand(40058,0);--P
-            table.insert(SelItemT_X,reaper.GetSelectedMediaItem(0,0));
-            reaper.Main_OnCommand(40058,0);--P
-            table.insert(SelItemT_X,reaper.GetSelectedMediaItem(0,0));
-            reaper.SelectAllMediaItems(0,0);
         end;
-        ---------------------------------------------
-    end;
     ----
-    reaper.SetEditCurPos(EditCursor_X,false,false);
-    reaper.GetSet_ArrangeView2(0,1,0,0,start_time_X,end_time_X);
-    if moveEnv_X then reaper.Main_OnCommand(40070,0)end;
-    
-    for i = 1, #SelItemT_X do;
-        reaper.SetMediaItemInfo_Value(SelItemT_X[i],"B_UISEL",1);
+    end;
+    ::END::
+    if itClkMvCr&8 ~= 0 then;
+       reaper.SNM_SetIntConfigVar("itemclickmovecurs",itClkMvCr);
     end;
     
-    if #SelTrackT_X > 0 then;
-        reaper.SetOnlyTrackSelected(SelTrackT_X[1]);
-    else;
-        reaper.SetOnlyTrackSelected(reaper.GetTrack(0,0));
-        reaper.SetMediaTrackInfo_Value(reaper.GetTrack(0,0),"I_SELECTED",0);
-    end;
-    
-    for i = 1, #SelTrackT_X do;
-        local Sel = reaper.GetMediaTrackInfo_Value(SelTrackT_X[i],"I_SELECTED");
-        if Sel == 0 then;
-            reaper.SetMediaTrackInfo_Value(SelTrackT_X[i],"I_SELECTED",1);
-        end;
-    end;
     --clo2 = os.clock()-clo1
     reaper.PreventUIRefresh(-1);
     reaper.Undo_EndBlock("Stutter 1/3 items",-1);
     --===========================================
-	reaper.UpdateArrange();
+    reaper.UpdateArrange();
