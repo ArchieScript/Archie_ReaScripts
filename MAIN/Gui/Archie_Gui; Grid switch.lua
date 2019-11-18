2dd43561dbf85bccf0a0eedd7147abf9b1b869d5
ArchieScript <ArchieScript@users.noreply.github.com>
2019-11-18 18:33:42 +0800

    +

---
+++ MAIN/Gui/Archie_Gui; Grid switch.lua
@@ -0,0 +1,37 @@
+--[[
+   * Тест только на windows  /  Test only on windows.
+   * Отчет об ошибке: Если обнаружите какие либо ошибки, то сообщите по одной из указанных ссылок ниже (*Website)
+   * Bug Reports: If you find any errors, please report one of the links below (*Website)
+   *
+   * Category:    Gui
+   * Description: Grid switch
+   * Author:      Archie
+   * Version:     1.03
+   * Описание:    Переключатель сетки
+   * Website:     http://forum.cockos.com/showthread.php?t=212819
+   *              http://rmmedia.ru/threads/134701/
+   * DONATION:    http://money.yandex.ru/to/410018003906628
+   * Customer:    Krikets(Rmm)
+   * Gave idea:   Krikets(Rmm)
+   * Extension:   Reaper 5.981+ http://www.reaper.fm/
+   *              SWS v.2.10.0 http://www.sws-extension.org/index.php
+   *              reaper_js_ReaScriptAPI64 Repository - (ReaTeam Extensions) http://clck.ru/Eo5Nr or http://clck.ru/Eo5Lw
+   * Changelog:   
+   *              v.1.02 [12.10.19]
+   *                  + Close window when focus is lost
+   *                  + When click button close window
+   
+   *              v.1.0 [10.10.19]
+   *                  + initialе
+--]]
+    
+    --======================================================================================
+    --////////////// SCRIPT \\\\\\\\\\\\\\  SCRIPT  //////////////  SCRIPT  \\\\\\\\\\\\\\\\
+    --======================================================================================
+    
+    
+    
+    local path,scr = ({reaper.get_action_context()})[2]:match("(.+)[/\\](.+)");
+    local scr = scr:gsub(";","D;");
+    local path = path:gsub("\\","/");
+    dofile(path.."/"..scr);
