da2deb4ffddc5a04936029e7b2db97aea0054ea2
ArchieScript <36672393+ArchieScript@users.noreply.github.com>
2020-02-06 16:59:04 +0800

    +

--- MAIN/Various/Archie_Var;  Open MIDI editor and zoom to content by.lua
+++ MAIN/Various/Archie_Var;  Open MIDI editor and zoom to content by.lua
@@ -6,7 +6,7 @@
    * Category:    Various
    * Description: Open MIDI editor and zoom to content by
    * Author:      Archie
-   * Version:     1.0
+   * Version:     1.02
    * Описание:    Откройте MIDI-редактор и увеличьте масштаб содержимого
    * Website:     http://forum.cockos.com/showthread.php?t=212819
    *              http://rmmedia.ru/threads/134701/
@@ -18,8 +18,14 @@
    * Provides:    
    *              [main] . > Archie_Var;  Open MIDI editor and zoom to content by item.lua
    *              [main] . > Archie_Var;  Open MIDI editor and zoom to content by source.lua
+   *              [main] . > Archie_Var;  Toggle Open MIDI editor and zoom to content by item.lua
+   *              [main] . > Archie_Var;  Toggle Open MIDI editor and zoom to content by source.lua
    *         
-   * Changelog:   v.1.0 [06.02.20]
+   * Changelog:   
+   *              v.1.02 [06.02.20]
+   *                  + Toggle
+   
+   *              v.1.0 [06.02.20]
    *                  + initialе
 --]]
      
@@ -45,58 +51,83 @@
     -------------------------------------------------------
     
     
+    --==============================================================
     local itemF_SourceT; -- itemLen-false/SourceLen-true
     local filename = ({reaper.get_action_context()})[2]:upper();
     if filename:match('^.*(SOURCE)%s-%.%s-LUA%s-$')then;
         itemF_SourceT = true;
     end;
     
-     
-    local ZoomProj = 40726;
-    local OpenMIDI = 40153;
-    local ZoomCont = 40466
-    
-    if not tonumber(Indent) then Indent = 0 end;
-    
-    reaper.Main_OnCommand(OpenMIDI,0);
-
-    local MIDIEditor = reaper.MIDIEditor_GetActive();
-    if not MIDIEditor then no_undo() return end;
-    
-    local Take = reaper.MIDIEditor_GetTake(MIDIEditor);
-    local Item = reaper.GetMediaItemTake_Item(Take);
-    local posItem = reaper.GetMediaItemInfo_Value(Item, "D_POSITION");
-    local lenItem = reaper.GetMediaItemInfo_Value(Item, "D_LENGTH");
-    local endPosItem = posItem + lenItem;
-    local StartLoop,EndLoop = reaper.GetSet_LoopTimeRange(0,1,0,0,0);
-    
-    local source = reaper.GetMediaItemTake_Source(Take);
-    local retval,lengthIsQN = reaper.GetMediaSourceLength(source);
-    local TimeFromQN = reaper.TimeMap2_QNToTime(0,retval);
-    --if TimeFromQN ~= len then Indent = 0 end;
-    
-    
-    
-    reaper.PreventUIRefresh(1);
-    
-    if Vertically_Zoom == true then;
-        reaper.MIDIEditor_OnCommand(MIDIEditor,ZoomCont);
+    local TOGGLE;
+    if filename:match('.+[/\\](.+)'):match('^ARCHIE_VAR;%s-%s-TOGGLE')then;
+        TOGGLE = true;
     end;
+    --==============================================================
     
     
-    if itemF_SourceT == true then;
-       local EndPosSrc = (posItem+TimeFromQN);
-       if EndPosSrc > endPosItem then EndPosSrc = endPosItem end;
-       if Indent >= (EndPosSrc-posItem)/2 then Indent = 0 end;
-       reaper.GetSet_LoopTimeRange(1,1,posItem+Indent,EndPosSrc-Indent,0);
+    --==============================================================
+    local function OpenMIDI_editor_and_zoom_to_content(itemF_SourceT)-- itemLen-false/SourceLen-true
+        local ZoomProj = 40726;
+        local OpenMIDI = 40153;
+        local ZoomCont = 40466
+        
+        if not tonumber(Indent) then Indent = 0 end;
+        
+        reaper.Main_OnCommand(OpenMIDI,0);
+    
+        local MIDIEditor = reaper.MIDIEditor_GetActive();
+        if not MIDIEditor then no_undo() return end;
+        
+        local Take = reaper.MIDIEditor_GetTake(MIDIEditor);
+        local Item = reaper.GetMediaItemTake_Item(Take);
+        local posItem = reaper.GetMediaItemInfo_Value(Item, "D_POSITION");
+        local lenItem = reaper.GetMediaItemInfo_Value(Item, "D_LENGTH");
+        local endPosItem = posItem + lenItem;
+        local StartLoop,EndLoop = reaper.GetSet_LoopTimeRange(0,1,0,0,0);
+        
+        local source = reaper.GetMediaItemTake_Source(Take);
+        local retval,lengthIsQN = reaper.GetMediaSourceLength(source);
+        local TimeFromQN = reaper.TimeMap2_QNToTime(0,retval);
+        --if TimeFromQN ~= len then Indent = 0 end;
+        
+        
+        reaper.PreventUIRefresh(1);
+        
+        if Vertically_Zoom == true then;
+            reaper.MIDIEditor_OnCommand(MIDIEditor,ZoomCont);
+        end;
+        
+        
+        if itemF_SourceT == true then;
+           local EndPosSrc = (posItem+TimeFromQN);
+           if EndPosSrc > endPosItem then EndPosSrc = endPosItem end;
+           if Indent >= (EndPosSrc-posItem)/2 then Indent = 0 end;
+           reaper.GetSet_LoopTimeRange(1,1,posItem+Indent,EndPosSrc-Indent,0);
+        else;
+            if Indent >= lenItem/2 then Indent = 0 end;
+            reaper.GetSet_LoopTimeRange(1,1,posItem+Indent,endPosItem-Indent,0);
+        end;
+        
+        reaper.MIDIEditor_OnCommand(MIDIEditor,ZoomProj);
+        reaper.GetSet_LoopTimeRange(1,1,StartLoop,EndLoop,0);
+        
+        reaper.PreventUIRefresh(-1);
+        reaper.UpdateTimeline();
+        no_undo();
+    end;
+    --==============================================================
+    
+    
+    
+    --------------------------------------------------------
+    if TOGGLE == true then;
+        local MIDIEditor = reaper.MIDIEditor_GetActive();
+        if MIDIEditor then;
+            reaper.MIDIEditor_OnCommand(MIDIEditor,2);
+        else;
+            OpenMIDI_editor_and_zoom_to_content(itemF_SourceT);
+        end;
     else;
-        if Indent >= lenItem/2 then Indent = 0 end;
-        reaper.GetSet_LoopTimeRange(1,1,posItem+Indent,endPosItem-Indent,0);
+        OpenMIDI_editor_and_zoom_to_content(itemF_SourceT);
     end;
-    
-    reaper.MIDIEditor_OnCommand(MIDIEditor,ZoomProj);
-    reaper.GetSet_LoopTimeRange(1,1,StartLoop,EndLoop,0);
-    
-    reaper.PreventUIRefresh(-1);
-    reaper.UpdateTimeline();
-    no_undo();
+    --------------------------------------------------------
