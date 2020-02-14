a43d08d84fe63bb52c95920860c970c2a60aa5ac
ArchieScript <36672393+ArchieScript@users.noreply.github.com>
2020-02-15 01:49:05 +0800

    +

--- MAIN/Item/Archie_Item;  Copy stretch markers.lua
+++ MAIN/Item/Archie_Item;  Copy stretch markers.lua
@@ -12,8 +12,8 @@
    * Website:     http://forum.cockos.com/showthread.php?t=212819
    *              http://rmmedia.ru/threads/134701/
    * DONATION:    http://money.yandex.ru/to/410018003906628
-   * Customer:    Борис Янушевич(VK)$
-   * Gave idea:   Борис Янушевич(VK)$
+   * Customer:    borisuperful(RMM)$
+   * Gave idea:   borisuperful(RMM)$
    * Extension:   Reaper 6.03+ http://www.reaper.fm/
    *              SWS v.2.10.0 http://www.sws-extension.org/index.php
    * Changelog:   
