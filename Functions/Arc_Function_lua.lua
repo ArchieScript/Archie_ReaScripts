d20d9997f4677355beb82d6ae27cdb01fe989806
ArchieScript <36672393+ArchieScript@users.noreply.github.com>
2020-02-14 13:54:45 +0800

    +

--- Functions/Arc_Function_lua.lua
+++ Functions/Arc_Function_lua.lua
@@ -1,9 +1,9 @@
-local VersionMod = "v.2.7.5"
+local VersionMod = "v.2.7.6"
 --[[
    * Category:    Function
    * Description: Arc_Function_lua
    * Author:      Archie
-   * Version:     2.7.5
+   * Version:     2.7.6
    * AboutScript: Functions for use with some scripts Archie
    * О скрипте:   Функции для использования с некоторыми скриптами Archie
    * Provides:    [nomain].
