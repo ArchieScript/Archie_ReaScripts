07e647869b3fe192a2873acd2f3b7da8b1ef4670
ArchieScript <36672393+ArchieScript@users.noreply.github.com>
2020-02-18 14:56:57 +0800

    +

--- MAIN/Mixer/Archie_Mixer;  Reset height all tracks MCP by master track.lua
+++ MAIN/Mixer/Archie_Mixer;  Reset height all tracks MCP by master track.lua
@@ -3,10 +3,10 @@
    * Отчет об ошибке: Если обнаружите какие либо ошибки, то сообщите по одной из указанных ссылок ниже (*Website)
    * Bug Reports: If you find any errors, please report one of the links below (*Website)
    *
-   * Description: Mixer;  Reset view selected tracks MCP by master track
+   * Description: Mixer;  Reset height all tracks MCP by master track
    * Author:      Archie
    * Version:     1.0
-   * Описание:    Микшер;  Сбросить вид невыбранных дорожек MCP по главной дорожке
+   * Описание:    Микшер;  Сбросить высоту всех дорожек MCP по главной дорожке
    * Website:     http://forum.cockos.com/showthread.php?t=212819
    *              http://rmmedia.ru/threads/134701/
    * DONATION:    http://money.yandex.ru/to/410018003906628
@@ -46,31 +46,26 @@
         
         for i = 1,CountTrack do;
             local track = reaper.GetTrack(0,i-1);
-            local sel = reaper.GetMediaTrackInfo_Value(track,'I_SELECTED');
-            if sel == 0 then;
-                ----
-                local retval, str = reaper.GetTrackStateChunk(track,'',false);
-                local str2 = str:match('SHOWINMIX.-\n');
-                
-                local x = -1;
-                local STR = '';
-                for val in string.gmatch(str2,"%S+") do;
-                   x = x + 1;
-                   if x == 2 then val = trHeight end;
-                   if x == 3 then val = Fxparam end;
-                   if x == 5 then val = trInsertFx end;
-                   STR = STR..val..' ';
-                end;
-                
-                STR = str:gsub('SHOWINMIX.-\n',STR..'\n'); 
-                
-                reaper.SetTrackStateChunk(track,STR,false);
-                ------
+            local retval, str = reaper.GetTrackStateChunk(track,'',false);
+            local str2 = str:match('SHOWINMIX.-\n');
+            
+            local x = -1;
+            local STR = '';
+            for val in string.gmatch(str2,"%S+") do;
+               x = x + 1;
+               if x == 2 then val = trHeight end;
+               if x == 3 then val = Fxparam end;
+               if x == 5 then val = trInsertFx end;
+               STR = STR..val..' ';
             end;
+            
+            STR = str:gsub('SHOWINMIX.-\n',STR..'\n'); 
+            
+            reaper.SetTrackStateChunk(track,STR,false);
         end;
         
         reaper.PreventUIRefresh(-1);
-        reaper.Undo_EndBlock('Reset view unselected tracks MCP by master track',-1);
+        reaper.Undo_EndBlock('Reset height all tracks MCP by master track',-1);
     end;
     
     
--- MAIN/Mixer/Archie_Mixer;  Reset height unselected tracks MCP by master track.lua
+++ MAIN/Mixer/Archie_Mixer;  Reset height unselected tracks MCP by master track.lua
@@ -3,10 +3,10 @@
    * Отчет об ошибке: Если обнаружите какие либо ошибки, то сообщите по одной из указанных ссылок ниже (*Website)
    * Bug Reports: If you find any errors, please report one of the links below (*Website)
    *
-   * Description: Mixer;  Reset view selected tracks MCP by master track
+   * Description: Mixer;  Reset height selected tracks MCP by master track
    * Author:      Archie
    * Version:     1.0
-   * Описание:    Микшер;  Сбросить вид выбранных дорожек MCP по главной дорожке
+   * Описание:    Микшер;  Сбросить высоту невыбранных дорожек MCP по главной дорожке
    * Website:     http://forum.cockos.com/showthread.php?t=212819
    *              http://rmmedia.ru/threads/134701/
    * DONATION:    http://money.yandex.ru/to/410018003906628
@@ -28,8 +28,8 @@
     -------------------------------------------------------
     
     
-    local CountSelTrack = reaper.CountSelectedTracks(0);
-    if CountSelTrack == 0 then no_undo() return end;
+    local CountTrack = reaper.CountTracks(0);
+    if CountTrack == 0 then no_undo() return end;
     
     
     local MasterTrack = reaper.GetMasterTrack(0);
@@ -44,30 +44,33 @@
         reaper.Undo_BeginBlock();
         reaper.PreventUIRefresh(1);
         
-        for i = 1,CountSelTrack do;
-            local track = reaper.GetSelectedTrack(0,i-1);
-            ----
-            local retval, str = reaper.GetTrackStateChunk(track,'',false);
-            local str2 = str:match('SHOWINMIX.-\n');
-            
-            local x = -1;
-            local STR = '';
-            for val in string.gmatch(str2,"%S+") do;
-               x = x + 1;
-               if x == 2 then val = trHeight end;
-               if x == 3 then val = Fxparam end;
-               if x == 5 then val = trInsertFx end;
-               STR = STR..val..' ';
+        for i = 1,CountTrack do;
+            local track = reaper.GetTrack(0,i-1);
+            local sel = reaper.GetMediaTrackInfo_Value(track,'I_SELECTED');
+            if sel == 0 then;
+                ----
+                local retval, str = reaper.GetTrackStateChunk(track,'',false);
+                local str2 = str:match('SHOWINMIX.-\n');
+                
+                local x = -1;
+                local STR = '';
+                for val in string.gmatch(str2,"%S+") do;
+                   x = x + 1;
+                   if x == 2 then val = trHeight end;
+                   if x == 3 then val = Fxparam end;
+                   if x == 5 then val = trInsertFx end;
+                   STR = STR..val..' ';
+                end;
+                
+                STR = str:gsub('SHOWINMIX.-\n',STR..'\n'); 
+                
+                reaper.SetTrackStateChunk(track,STR,false);
+                ------
             end;
-            
-            STR = str:gsub('SHOWINMIX.-\n',STR..'\n'); 
-            
-            reaper.SetTrackStateChunk(track,STR,false);
-            ------
         end;
         
         reaper.PreventUIRefresh(-1);
-        reaper.Undo_EndBlock('Reset view selected tracks MCP by master track',-1);
+        reaper.Undo_EndBlock('Reset height unselected tracks MCP by master track',-1);
     end;
     
     
