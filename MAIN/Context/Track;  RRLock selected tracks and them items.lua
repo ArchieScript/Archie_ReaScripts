98756be857a13d9f2fa0f95f36bd0af4a18dc14c
ArchieScript <36672393+ArchieScript@users.noreply.github.com>
2020-02-26 21:50:57 +0800

    +

---
+++ MAIN/Context/Track;  RRLock selected tracks and them items.lua
@@ -0,0 +1,22 @@
+--[[
+   * Тест только на windows  /  Test only on windows.
+   * Отчет об ошибке: Если обнаружите какие либо ошибки, то сообщите по одной из указанных ссылок ниже (*Website)
+   * Bug Reports: If you find any errors, please report one of the links below (*Website)
+   *
+   * Category:    Track
+   * Description: Track;  RRLock selected tracks and them items
+   *                      (Unlock selected tracks and restore them items)
+   * Author:      Archie
+   * Version:     1.0
+   * Описание:    Блокировка выбранных треков и их элементов
+   * Website:     http://forum.cockos.com/showthread.php?t=212819
+   *              http://rmmedia.ru/threads/134701/
+   * DONATION:    http://money.yandex.ru/to/410018003906628
+   * Customer:    Archie(---)$
+   * Gave idea:   Archie(---)$
+   * Extension:   Reaper 6.03+ http://www.reaper.fm/
+   *              SWS v.2.10.0 http://www.sws-extension.org/index.php
+   * Changelog:   
+   *              v.1.0 [26.02.20]
+   *                  + initialе
+--]]
