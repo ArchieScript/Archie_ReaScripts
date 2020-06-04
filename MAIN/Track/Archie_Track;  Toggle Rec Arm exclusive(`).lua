136e2b897fc0e298a402e25449371a7503d7e0d1
ArchieScript <36672393+ArchieScript@users.noreply.github.com>
2020-06-04 21:18:41 +0800

    +

--- MAIN/Track/Archie_Track;  Toggle Rec Arm exclusive(`).lua
+++ MAIN/Track/Archie_Track;  Toggle Rec Arm exclusive(`).lua
@@ -6,13 +6,13 @@
    * Category:    Track
    * Description: Track;  Toggle Rec Arm exclusive(`).lua
    * Author:      Archie
-   * Version:     1.0
+   * Version:     1.02
    * Website:     http://forum.cockos.com/showthread.php?t=212819
    *              http://rmmedia.ru/threads/134701/
    *              http://vk.com/reaarchie
    * DONATION:    http://money.yandex.ru/to/410018003906628
-   * Customer:    AlexLazer(rmm)
-   * Gave idea:   AlexLazer(rmm)
+   * Customer:    AlexLazer(rmm)$
+   * Gave idea:   AlexLazer(rmm)$
    * Extension:   Reaper 6.10+ http://www.reaper.fm/
    *              SWS v.2.12.0 http://www.sws-extension.org/index.php
    * Changelog:   
