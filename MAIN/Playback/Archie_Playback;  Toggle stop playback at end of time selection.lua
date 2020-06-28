5c68010709158da6414a7e06788f36152baecf4d
ArchieScript <36672393+ArchieScript@users.noreply.github.com>
2020-06-28 15:50:10 +0800

    +

--- MAIN/Playback/Archie_Playback;  Toggle stop playback at end of time selection.lua
+++ MAIN/Playback/Archie_Playback;  Toggle stop playback at end of time selection.lua
@@ -1,43 +1,33 @@
---[[
+--[[        TERMINATE INSTANCES
+   * Тест только на windows  /  Test only on windows.
+   * Отчет об ошибке: Если обнаружите какие либо ошибки, то сообщите по одной из указанных ссылок ниже (*Website)
+   * Bug Reports: If you find any errors, please report one of the links below (*Website)
+   *
    * Category:    Playback
-   * Description: Toggle stop playback at the end of time selection
+   * Description: Playback;  stop playback at the end of time selection.lua
    * Author:      Archie
-   * Version:     1.04
-   * AboutScript: stop playback at the end of time selection
+   * Version:     1.05
    * О скрипте:   остановить воспроизведения в конце выбора времени
-   * GIF:         ---
    * Website:     http://forum.cockos.com/showthread.php?t=212819
    *              http://rmmedia.ru/threads/134701/
-   * Donation:    http://money.yandex.ru/to/410018003906628
+   *              http://vk.com/reaarchie
+   * DONATION:    http://money.yandex.ru/to/410018003906628
    * Customer:    Supa75(RMM)
    * Gave idea:   Supa75(RMM)
-   * Changelog:   +  Fixed paths for Mac/ v.1.03 [29.01.19] 
-   *              +  Исправлены пути для Mac/ v.1.03 [29.01.19]  
-
-   *              +  initialе / v.1.0 [22.01.2019]
-   
-   ===========================================================================================\
-   -------------SYSTEM REQUIREMENTS:-------/-------СИСТЕМНЫЕ ТРЕБОВАНИЯ:----------------------|
-   ===========================================================================================|
-   + Reaper v.5.965 -----------| http://www.reaper.fm/download.php -------|(and above |и выше)|
-   + SWS v.2.9.7 --------------| http://www.sws-extension.org/index.php --|(and above |и выше)|
-   - ReaPack v.1.2.2 ----------| http://reapack.com/repos ----------------|(and above |и выше)|
-   + Arc_Function_lua v.2.2.2 -| Repository - Archie-ReaScripts  http://clck.ru/EjERc |и выше)|
-   - reaper_js_ReaScriptAPI64 -| Repository - ReaTeam Extensions http://clck.ru/Eo5Nr |и выше)|
-                                                                 http://clck.ru/Eo5Lw |и выше)|
-   - Visual Studio С++ 2015 ---| --------- http://clck.ru/Eq5o6 ----------|(and above |и выше)|
---===========================================================================================]]
-
-
-
-
+   * Provides:
+   * Extension:   Reaper 6.12+ http://www.reaper.fm/
+   *              SWS v.2.10.0 http://www.sws-extension.org/index.php
+   *              Arc_Function_lua v.2.8.5+  (Repository: Archie-ReaScripts) http://clck.ru/EjERc
+   * Changelog:   
+   *              v.1.0 [220119]
+   *                  + initialе
+--]] 
     --======================================================================================
     --////////////// SCRIPT \\\\\\\\\\\\\\  SCRIPT  //////////////  SCRIPT  \\\\\\\\\\\\\\\\
     --======================================================================================
-
-
-
-
+    
+    
+    
     --=========================================
     local function MODULE(file);
         local E,A=pcall(dofile,file);if not(E)then;reaper.ShowConsoleMsg("\n\nError - "..debug.getinfo(1,'S').source:match('.*[/\\](.+)')..'\nMISSING FILE / ОТСУТСТВУЕТ ФАЙЛ!\n'..file:gsub('\\','/'))return;end;
@@ -45,24 +35,15 @@
     end; local Arc = MODULE((reaper.GetResourcePath()..'/Scripts/Archie-ReaScripts/Functions/Arc_Function_lua.lua'):gsub('\\','/'));
     if not Arc then return end;
     --=========================================
-	
-
-
-
-
-    local function SetToggleButtonOnOff(numb);
-        local value,ScriptWay,sec,cmd,mod,res,val = reaper.get_action_context();
-        reaper.SetToggleCommandState(sec,cmd,numb or 0);
-        reaper.RefreshToolbar2(sec,cmd);
-    end;
-
-    Arc.HelpWindowWhenReRunning(1,14541,false);
+    
+    
+     
     local Jump,Active = 9^99;
     local function loop();
-         
-        local startLoop, endLoop = reaper.GetSet_LoopTimeRange(0,0,0,0,0);
-        if startLoop ~= endLoop then;    
-
+        
+        local startLoop,endLoop = reaper.GetSet_LoopTimeRange(0,0,0,0,0);
+        if startLoop ~= endLoop then;
+            
             if not Active then;    
                 local PlayState = reaper.GetPlayState();
                 local startLoop, endLoop = reaper.GetSet_LoopTimeRange(0,0,0,0,0);
@@ -73,7 +54,7 @@
                     end;
                 end;
             end;
-
+            
             if Active then;
                  local PlayState = reaper.GetPlayState();
                  local startLoop, endLoop = reaper.GetSet_LoopTimeRange(0,0,0,0,0);
@@ -93,7 +74,15 @@
         end;
         reaper.defer(loop);
     end;
-
-    SetToggleButtonOnOff(1);
+    
+    
+    Arc.SetToggleButtonOnOff(1,1);
     loop();
-    reaper.atexit(SetToggleButtonOnOff);
+    reaper.atexit(function()Arc.SetToggleButtonOnOff(0,1)end);
+    
+    
+    ---------------------
+    reaper.defer(function();local ScrPath,ScrName = debug.getinfo(1,'S').source:match('^[@](.+)[/\\](.+)');Arc.GetSetTerminateAllInstancesOrStartNewOneKB_ini(1,260,ScrPath,ScrName)end);
+    
+    
+    
