10ea48a799ba961dd90c0f22cccc683448b27524
ArchieScript <36672393+ArchieScript@users.noreply.github.com>
2020-08-08 19:05:26 +0800

    +

--- MAIN/Markers/Archie_Markers; Toggle Insert-Delete marker in cursor position.lua
+++ MAIN/Markers/Archie_Markers; Toggle Insert-Delete marker in cursor position.lua
@@ -4,7 +4,7 @@
    * Bug Reports: If you find any errors, please report one of the links below (*Website)
    *
    * Category:    Markers
-   * Description: Markers; Insert-Delete marker in cursor position.lua
+   * Description: Markers; Toggle Insert-Delete marker in cursor position.lua
    * Author:      Archie
    * Version:     1.0
    * Website:     http://forum.cockos.com/showthread.php?t=212819
