166553fa522f76caca141946d7ffc9bc8d31214b
ArchieScript <36672393+ArchieScript@users.noreply.github.com>
2020-09-27 14:21:29 +0800

    +

--- MAIN/Edit cursor/Archie_Edit cursor; Go To Time(time).lua
+++ MAIN/Edit cursor/Archie_Edit cursor; Go To Time(time).lua
@@ -6,7 +6,7 @@
    * Category:    Edit cursor
    * Description: Edit cursor; Go To Time(time).lua
    * Author:      Archie
-   * Version:     1.02
+   * Version:     1.03
    * Website:     http://forum.cockos.com/showthread.php?t=212819
    *              http://rmmedia.ru/threads/134701/
    *              http://vk.com/reaarchie
@@ -24,10 +24,7 @@
     --////////////  НАСТРОЙКИ  \\\\\\\\\\\\  SETTINGS  ////////////  НАСТРОЙКИ  \\\\\\\\\\\\
     --======================================================================================
     
-    local
-    Previous_input = false;
-                -- = false | В окне ввода показать текущее положение курсора
-                -- = true  | В окне ввода показать предыдущее введенное значение
+    local MODE = 1 -- 0 / 1
     
     --======================================================================================
     --////////////// SCRIPT \\\\\\\\\\\\\\  SCRIPT  //////////////  SCRIPT  \\\\\\\\\\\\\\\\
@@ -39,35 +36,74 @@
     local function no_undo()reaper.defer(function()end)end;
     -------------------------------------------------------
     
-    
     local title = 'Go To Time (time)';
-    
-    local _,filename,buf;
-    
-    if Previous_input == true then;
-        _,filename,_,_,_,_,_ = reaper.get_action_context();
-        buf = reaper.GetExtState(filename,title);
-    end;
-    
-    if not buf or buf == '' then;
-        local CurPos = reaper.GetCursorPosition();
-        buf = reaper.format_timestr_pos(CurPos,'',0);
-    end;
-    
-    local retval,retvals_csv = reaper.GetUserInputs(title,1,'Sec:,extrawidth=25',buf);
-    if not retval then no_undo()return end;
-    
-    local MSec = retvals_csv:match(                  '(%d+)$')or 0;
-    local Sec  = retvals_csv:match(            '(%d+)%D+%d*$')or 0;
-    local Min  = retvals_csv:match(      '(%d+)%D+%d*%D+%d*$')or 0;
-    local Hour = retvals_csv:match('(%d+)%D+%d*%D+%d*%D+%d*$')or 0;
-    
-    local time = reaper.parse_timestr_pos(Hour..':'..Min..':'..Sec..'.'..MSec,0);
-    reaper.SetEditCurPos(time,true,false);
+    local _,filename,_,_,_,_,_ = reaper.get_action_context();
+    local Mouse_x,Mouse_y = reaper.GetMousePosition();
     
     
-    if Previous_input == true then;
+    if MODE == 0 then;
+        ---------------------------------------------------------
+        local buf = tonumber(reaper.GetExtState(filename,title))or 0;
+        
+        local retval,retvals_csv = reaper.GetUserInputs(title,1,'Sec: ( H M S MS ),extrawidth=25',buf);
+        if not retval then no_undo()return end;
+        
+        local MSec = retvals_csv:match(                  '(%d+)$')or 0;
+        local Sec  = retvals_csv:match(            '(%d+)%D+%d*$')or 0;
+        local Min  = retvals_csv:match(      '(%d+)%D+%d*%D+%d*$')or 0;
+        local Hour = retvals_csv:match('(%d+)%D+%d*%D+%d*%D+%d*$')or 0;
+        
+        local time = reaper.parse_timestr_pos(Hour..':'..Min..':'..Sec..'.'..MSec,0);
+        reaper.SetEditCurPos(time,true,false);
+        
         reaper.SetExtState(filename,title,retvals_csv,false);
+        ---------------------------------------------------------
+    else;
+        ---------------------------------------------------------
+        local Hour = tonumber(reaper.GetExtState(filename,'Hour'))or 0;
+        local Min  = tonumber(reaper.GetExtState(filename,'Min' ))or 0;
+        local Sec  = tonumber(reaper.GetExtState(filename,'Sec' ))or 0;
+        local MSec = tonumber(reaper.GetExtState(filename,'MSec'))or 0;
+        
+        local
+        retval,retvals_csv = reaper.GetUserInputs(title,4,'Hour: '..(' -'):rep(40)..','..
+                                                          'Minutes:  '..(' -'):rep(40)..','..
+                                                          'Sec:  '..(' -'):rep(40)..','..
+                                                          'Milliseconds  '..(' -'):rep(40)..','..
+                                                          'extrawidth=25,separator=$',
+                                                          Hour..'$'..Min..'$'..Sec..'$'..MSec);
+        if not retval then no_undo()return end;------------
+        retvals_csv = retvals_csv..'$';
+        
+        if #retvals_csv:gsub('[^$]','')> 4 then;
+            reaper.TrackCtl_SetToolTip('---$---\nERROR\n-------',Mouse_x,Mouse_y,false);
+            no_undo()return;
+        end;
+        
+        
+        Hour,Min,Sec,MSec = retvals_csv:match('^(.-)%$(.-)%$(.-)%$(.-)%$');
+        
+        Hour = tonumber(Hour)or 0;
+        Min  = tonumber(Min) or 0;
+        Sec  = tonumber(Sec) or 0;
+        MSec = tonumber(MSec)or 0;
+        
+        local time = reaper.parse_timestr_pos(Hour..':'..Min..':'..Sec..'.'..MSec,0);
+        reaper.SetEditCurPos(time,true,false);
+        
+        reaper.SetExtState(filename,'Hour',Hour,false);
+        reaper.SetExtState(filename,'Min' ,Min ,false);
+        reaper.SetExtState(filename,'Sec' ,Sec ,false);
+        reaper.SetExtState(filename,'MSec',MSec,false);
+        ---------------------------------------------------------
     end;
     
     no_undo();
+    
+    
+    
+    
+    
+    
+    
+    
