f3d7bb341ef67a5ee1790480c6480f5234722e10
ArchieScript <36672393+ArchieScript@users.noreply.github.com>
2020-10-10 23:45:03 +0800

    +

---
+++ MAIN/Time selection/Archie_Time selection; Set loop by time selection.lua
@@ -0,0 +1,40 @@
+--[[
+   * Тест только на windows  /  Test only on windows.
+   * Отчет об ошибке: Если обнаружите какие либо ошибки, то сообщите по одной из указанных ссылок ниже (*Website)
+   * Bug Reports: If you find any errors, please report one of the links below (*Website)
+   *
+   * Category:    Time selection
+   * Description: Time selection; Set loop by time selection.lua
+   * Author:      Archie
+   * Version:     1.0
+   * AboutScript: ---
+   * О скрипте:   ---
+   * GIF:         ---
+   * Website:     http://forum.cockos.com/showthread.php?t=212819
+   *              http://rmmedia.ru/threads/134701/
+   *              http://vk.com/reaarchie
+   * DONATION:    http://money.yandex.ru/to/410018003906628
+   * DONATION:    http://paypal.me/ReaArchie?locale.x=ru_RU
+   * Customer:    Snjuk(Rmm)
+   * Gave idea:   Snjuk(Rmm)
+   * Extension:   Reaper 6.14+ http://www.reaper.fm/
+   *              SWS v.2.12.0 http://www.sws-extension.org/index.php
+   * Changelog:   
+   *              v.1.0 [101020]
+   *                  + initialе
+--]]
+    --======================================================================================
+    --////////////// SCRIPT \\\\\\\\\\\\\\  SCRIPT  //////////////  SCRIPT  \\\\\\\\\\\\\\\\
+    --======================================================================================
+    
+    
+    reaper.defer(function(); 
+        local Loopstart,LoopEnd = reaper.GetSet_LoopTimeRange(0,1,0,0,0);
+        local timeSelStart,timeSelEnd = reaper.GetSet_LoopTimeRange(0,0,0,0,0);
+        if Loopstart~=timeSelStart or LoopEnd~=timeSelEnd then;
+            reaper.GetSet_LoopTimeRange(1,1,timeSelStart,timeSelEnd,0);
+        end;
+    end);
+    
+    
+    
